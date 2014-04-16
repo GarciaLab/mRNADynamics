c175bb7e9414853d25dbf4dd5f3b870068e72562
Jacques Bothma Desktop <jpbothma@gmail.com>
2014-04-16 15:50:07 -0700

    Minor Changes

--- AnalyzeLiveDataDVFirstStep.m
+++ AnalyzeLiveDataDVFirstStep.m
@@ -1,4 +1,4 @@
-function AnalyzeLiveDataDV
+function AnalyzeLiveDataDVFirstStep
 
 %This function does all the required data exporting and runs the FISH
 %analysis code
@@ -22,78 +22,77 @@ function AnalyzeLiveDataDV
 %% Copy the images and do the initial FISH analysis
 
 
-% Process the raw images from the microscopes so that they can be analyzed
-% by the FISH code.
-% Prefix=ExportDataForFISH;
-% Optional parameters:
-% TAGOnly: Generate the TAG file only
-
-Prefix = '2013-12-28-kniBACHR';
-
-%  %Get the relevant folders now:
-  [SourcePath,FISHPath,DropboxFolder,MS2CodePath,SchnitzcellsFolder]=...
-      DetermineLocalFolders(Prefix);
-%  
-% 
-% 
-% %Start the matlab workers for the FISH analysis code
-% try
-%     matlabpool
-% catch
-%     display('matlabpool already running')
-%  end
-%  
-% 
-%  %First do an analysis without a threshold to generate the DoG images.
-%  cd([FISHPath,filesep,'Analysis'])
-%  analyzeShawnLibrary('fad',@(x)tagged(x,'id',[Prefix,'_']),'params_mRNADynamics',inf)
-%  cd([MS2CodePath])
-%  
+%Process the raw images from the microscopes so that they can be analyzed
+% %by the FISH code.
+ Prefix=ExportDataForFISH;
+%Optional parameters:
+%TAGOnly: Generate the TAG file only
 
 
-%%%%Look at the dog-filtered images and decide on a threshold to use
-
-%We will keep the threshold low and then increase it after the fact.
-
-%For LSM settings
-Threshold=350;   
+%Get the relevant folders now:
+[SourcePath,FISHPath,DropboxFolder,MS2CodePath,SchnitzcellsFolder]=...
+    DetermineLocalFolders(Prefix);
 
 
-% %For Princeton
-% Threshold=30;
+%Start the matlab workers for the FISH analysis code
+try
+    matlabpool
+catch
+    display('matlabpool already running')
+end
 
-%Now, do an analysis with an actual threshold
+%First do an analysis without a threshold to generate the DoG images.
 cd([FISHPath,filesep,'Analysis'])
-analyzeShawnLibrary('fad',@(x)tagged(x,'id',[Prefix,'_']),'params_mRNADynamics',Threshold)
+analyzeShawnLibrary('fad',@(x)tagged(x,'id',[Prefix,'_']),'params_mRNADynamics',inf)
 cd([MS2CodePath])
 
-%% Track the nuclei and check their segmentation
-TrackNuclei(Prefix)
 
-%Check the segmentation
-CheckNucleiSegmentation(Prefix)
-%If segmentation was modified then we need to rerun the tracking
-TrackNuclei(Prefix)
+% Look at the dog-filtered images and decide on a threshold to use
 
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%% Track the particles and check the tracking. We also have the option to
-%% check the nuclei tracking in this round.
-
-%Track the particles, the two numbers are Threshold1 and Threshold2
-TrackmRNADynamics(Prefix,400,400)
-
-
-CheckParticleTracking(Prefix)
-
-
-%% Done
-
-
-CompileParticles(Prefix,'ApproveAll')
-
-
-
+% %We will keep the threshold low and then increase it after the fact.
+% 
+% %For LSM settings
+%  Threshold=250;   
+% 
+% %For Princeton
+% Threshold=90;
+% 
+% 
+% 
+% %Now, do an analysis with an actual threshold
+% cd([FISHPath,filesep,'Analysis'])
+% analyzeShawnLibrary('fad',@(x)tagged(x,'id',[Prefix,'_']),'params_mRNADynamics',Threshold)
+% cd([MS2CodePath])
+% 
+% 
+% 
+% %% Track the nuclei and check their segmentation
+% TrackNuclei(Prefix)
+% 
+% %Check the segmentation
+% CheckNucleiSegmentation(Prefix)
+% %If segmentation was modified then we need to rerun the tracking
+% TrackNuclei(Prefix)
+% 
+% 
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% %% Track the particles and check the tracking. We also have the option to
+% %% check the nuclei tracking in this round.
+% 
+% %Track the particles, the two numbers are Threshold1 and Threshold2
+% TrackmRNADynamics(Prefix,300,300)
+% 
+% 
+% CheckParticleTracking(Prefix)
+% 
+% 
+% %% Done
+% 
+% 
+% CompileParticles(Prefix,'ApproveAll')
+% 
+% 
+% 
 % 
 %     
