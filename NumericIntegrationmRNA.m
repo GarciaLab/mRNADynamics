f86a8f993efb549a1edc4b1d876de4598c678991
Jacques Bothma Desktop <jpbothma@gmail.com>
2014-05-19 17:13:07 -0700

    Interpolation troubleshooting

--- NumericIntegrationmRNA.m
+++ NumericIntegrationmRNA.m
@@ -1,12 +1,12 @@
 
-function f = NumericIntegrationmRNA(t,y,halflife,ElongationTime,alpha,Time)
+function f = NumericIntegrationmRNA(t,y,halflife,ElongationTime,alpha,Time,metho)
 
 lambda=log(2)/halflife;
 
-if t-ElongationTime/2<0
+if (t-ElongationTime/2)<0
 alphaI=0;
 else
-alphaI = interp1(Time,alpha,t-ElongationTime/2);
+alphaI = interp1(Time,alpha,t-ElongationTime/2,metho);
 end
 
 f=alphaI-y*lambda;
