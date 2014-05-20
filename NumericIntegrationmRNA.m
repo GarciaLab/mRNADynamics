280e1444d7370dfa4277242c6321c44eeebf8b95
Jacques Bothma <jpbothma@gmail.com>
2014-05-19 21:32:29 -0700

    Switched to chip interpolation.
    Seems to give better interpolation than spline (no negative bits) and
    is faster.

--- NumericIntegrationmRNA.m
+++ NumericIntegrationmRNA.m
@@ -1,12 +1,12 @@
 
-function f = NumericIntegrationmRNA(t,y,halflife,ElongationTime,alpha,Time,metho)
+function f = NumericIntegrationmRNA(t,y,halflife,ElongationTime,alpha,Time)
 
 lambda=log(2)/halflife;
 
 if (t-ElongationTime/2)<0
 alphaI=0;
 else
-alphaI = interp1(Time,alpha,t-ElongationTime/2,metho);
+alphaI = pchip(Time,alpha,t-ElongationTime/2);
 end
 
 f=alphaI-y*lambda;
