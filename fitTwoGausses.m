acda544677e13f2e5c2d6462fb383b60cf35d436
ArmandoReimer <armandoreimer@gmail.com>
2016-07-01 14:43:35 -0700

    bug fix

--- fitTwoGausses.m
+++ fitTwoGausses.m
@@ -9,9 +9,15 @@ snip = CPsmooth(snip,'Gaussian Filter',1.3,0);
 snip = double(snip);
 [y,x] = meshgrid(1:size(snip,2), 1:size(snip,1));
 
-singleGaussian = @(params) params(1).*exp((-1/2).*(((x-params(2))./params(3)).^2 ...
-        + ((y-params(4))./params(5)).^2)) + params(6) - double(snip);
+% singleGaussian = @(params) params(1).*exp((-1/2).*(((x-params(2))./params(3)).^2 ...
+%         + ((y-params(4))./params(5)).^2)) + params(6) - double(snip);
 
+singleGaussian = @(params) params(1).*exp( -( ...
+    -( (cos(params(7))^2 / 2*params(3)^2) + (sin(params(7))^2 / 2*params(5)^2) )*(x-params(2)).^2 ...
+-2*( (sin(2*params(7)) / 4*params(3)^2) + (sin(2*params(7)) / 4*params(5)^2)) * (x-params(2)).*(y-params(4))...
+    + ( (sin(params(7))^2 / 2*params(3)^2) + (cos(params(7))^2 / 2*params(5)^2) ) *(y-params(4)).^2))...
+    + params(6) - double(snip);
+    
 doubleGaussian = @(params) params(1).*exp((-1/2).*(((x-params(2))./params(3)).^2 ... 
         + ((y-params(4))./params(5)).^2)) ... 
         + params(6).*exp((-1/2).*(((x-params(7))./params(8)).^2  ...
@@ -29,10 +35,10 @@ centers = double(step(hLocalMax, snip));
 
 if size(centers,1) == 1
     init_params = [max(max(snip)), centers(1,2), WidthGuess, ... 
-        centers(1,1), WidthGuess, OffsetGuess];
+        centers(1,1), WidthGuess, OffsetGuess, 0];
 
     [fits, res1, residual, exitflag, output, lambda, jacobian] = lsqnonlin(singleGaussian, ...
-        init_params,zeros(1,6),inf(1,6);
+        init_params,zeros(1,7),inf(1,7));
     
     ci = nlparci(fits,residual,'jacobian',jacobian); %compute 95% confidence interval from the regression
     errors = zeros(1, length(fits));
@@ -76,9 +82,9 @@ else
     % TODO: I'm just making this so the script doesn't crash when it can't
     % find any maxima. But it's ugly and should be replaced with something
     % like assigning NaNs to all the return values.
-    init_params = [2000, 10, 5, 10, 5,1000];
+    init_params = [2000, 10, 5, 10, 5,1000, 0];
     [fits, res1, residual, exitflag, output, lambda, jacobian] = lsqnonlin(singleGaussian, ...
-        init_params,zeros(1,6),inf(1,6));
+        init_params,zeros(1,7),inf(1,7));
     
     ci = nlparci(fits,residual,'jacobian',jacobian);
     errors = zeros(1, length(fits));
@@ -88,10 +94,11 @@ else
     rel_errors = abs(errors./fits);
 end
 
-%Compute intensities by integrating over the Gaussian fit
+%Compute intensities by integrating over the Gaussian fit. Offset
+%subtracted
 % TODO: consider the case with two gaussians here
 
-GaussianIntensity = sum(sum(singleGaussian(fits) + double(snip) - fits(end)));
+GaussianIntensity = sum(sum(singleGaussian(fits) + double(snip) - fits(6)));
 
 if show_status
     figure(2)
