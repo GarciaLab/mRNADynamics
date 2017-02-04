0fd6173b750758958f51b55ad8c40fe04464554c
Hernan Garcia <hggarcia@gmail.com>
2017-02-04 00:01:30 -0800

    BioFormats backwards compatibility

--- ExportDataForFISH.m
+++ ExportDataForFISH.m
@@ -914,8 +914,14 @@ elseif strcmp(FileMode,'LIFExport')
             FrameInfo(i).PixelsPerLine=str2double(LIFMeta.getPixelsSizeX(0));
             FrameInfo(i).NumberSlices=min(NSlices);
             FrameInfo(i).FileMode='LIFExport';
-            FrameInfo(i).PixelSize=str2num(LIFMeta.getPixelsPhysicalSizeX(0));
-            FrameInfo(i).ZStep=str2double(LIFMeta.getPixelsPhysicalSizeZ(0));
+            %This is to allow for backwards compatibility with BioFormats
+            if ~isempty(str2num(LIFMeta.getPixelsPhysicalSizeX(0)))
+                FrameInfo(i).PixelSize=str2num(LIFMeta.getPixelsPhysicalSizeX(0));
+                FrameInfo(i).ZStep=str2double(LIFMeta.getPixelsPhysicalSizeZ(0));
+            else
+                FrameInfo(i).PixelSize=str2num(LIFMeta.getPixelsPhysicalSizeX(0).value);
+                FrameInfo(i).ZStep=str2double(LIFMeta.getPixelsPhysicalSizeZ(0).value);
+            end
             FrameInfo(i).Time=InitialStackTime(i);
         end
         %Find the flat field (FF) information
