7623ae1c09fc3b8d2b25b07d70a17038191234ed
meghanturner16 <meghanturner62@gmail.com>
2017-06-04 18:44:01 -0700

    Created a template for a standard script documentation header
    While looking through our current code, I realized it would be very
    helpful to have a standard documentation header at the top of each
    script or function. Right now, everyone's scripts are documented in
    different ways, making it difficult to systematically compile
    information about all our code.
    
    I have added a script, "documentationTemplate", to the master
    mRNADynamics branch, which contains a template/example of a standard
    documentation header that everyone should put on their code. Please make
    an effort to both include the standard header in any new code you add
    and go back into any code you've written and put a proper header on it.
    
    The most important detail to note is that the header must be in a single
    comment block (i.e. if you want to add blank lines in between sections,
    you need to have a percent sign '%' at the start to make a commented
    line, as shown in the template). This is because we will likely be using
    MATLAB's own "help" and "doc" functions to compile documentation, and
    they pull everything in the first uninterrupted comment block. If you're
    unsure about this, feel free to ask me.
    
    I know this might take a bit of work, but this will make our lives
    easier in the future. If you have any questions about the header or any
    suggestions for additions/changes you think would be helpful, please let
    me know!

---
+++ documentationTemplate.m
@@ -0,0 +1,50 @@
+%[This is a template/example of the standard documentation. Anything that
+%appears in [] is a comment about the template or something that should be
+%replaced with the appropriate information about your code.]
+%[Created by Meghan & Armando on 6/1/17]
+
+function segmentSpotsTemp(Prefix,Threshold,varargin)
+%segmentSpotsTemp(Prefix, Threshold, [Options])
+%
+%DESCRIPTION
+%Identify and segregate individual transcription spots. [Make the
+%description as short or long as you feel is necessary to communicate the
+%purpose of the script/function.]
+%
+%PARAMETERS
+%Prefix: Prefix of the data set to analyze
+%Threshold: Threshold to be used. Should be kept at ~90-200 for lattice
+%           light-sheet data, and at ~5-10 for confocal data (Leica SP8).
+%           If left empty, then the code just generates the DoG files.
+%[Any other parameters you have]
+%
+%OPTIONS
+%[In this example, and many of our other scripts, the options are the
+%different parameters you can pass to the function (typically via varargin)
+%to adjust how the code runs.]
+%'displayFigures':   If you want to display plots and images.
+%                
+%'TrackSpots':   Do you want to use this code to track the particles instead
+%                of using TrackmRNADynamics? 
+%'Frames',N:     Run the code from frame 1 to frame N. Defaults to all
+%                frames. It's suggested to run 5-20 frames for debugging.
+%'NoShadows':    Spots without valid (peaked) z-profiles are normally
+%                discarded. This option overrides that.
+%[Any other options you may have]
+%
+%OUTPUT
+%[Any output that your script provides with a description of what it is. If 
+%applicable, please note where this output is saved.]
+%
+%Author (contact): Armando Reimer (areimer@berkeley.edu [Could also be slack])
+%Created: 01/01/2016
+%Last Updated: 12/31/2016
+
+%[Leave at least one blank line (without a comment symbol) before beginning
+%any additonal commenting or beginning the code. Everything contained in
+%the inital comment block will be included; everything after the first 
+%blank, uncommented line will be excluded. Thus, do not add any blank,
+%uncommented lines within the documentation header.]
+
+%[CODE]
+
