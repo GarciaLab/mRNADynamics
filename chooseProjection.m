699eee09b79668a3d57e263f0b798a3add6e3de2
emmaLuu <emma_luu@berkeley.edu>
2017-06-20 13:25:30 -0700

    Projection Features for CheckParticleTracking
    I added an option to switch between different projection modes: no
    projection (default mode), max z projection, median z projection and max
    time projection.  To use this option press '~' and it will pop up a
    dialog that will allow you to choose one of the four modes.

---
+++ chooseProjection.m
@@ -0,0 +1,25 @@
+function choice = chooseProjection
+
+    d = dialog('Position',[300 300 250 150],'Name','Select a Projection');
+    txt = uicontrol('Parent',d,'Style','text',...
+           'Position',[20 80 210 40],'String','Select a projection');
+       
+    popup = uicontrol('Parent',d,'Style','popup','Position',[75 70 100 25],...
+           'String',{'None (Default)';'Max Z';'Median Z'; 'Max Z and Time'},...
+           'Callback',@popup_callback);
+       
+    btn = uicontrol('Parent',d,'Position',[89 20 70 25],'String','Close',...
+           'Callback','delete(gcf)');
+       
+    choice = 'None (Default)';
+       
+    % Wait for d to close before running to completion
+    uiwait(d);
+   
+       function popup_callback(popup,event)
+          idx = popup.Value;
+          popup_items = popup.String;
+          % This code uses dot notation to get properties.
+          choice = char(popup_items(idx,:));
+       end
+end
