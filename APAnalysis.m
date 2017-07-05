219b5f17172445f85d8373dc9d6a0ad9d68ae13e
ArmandoReimer <areimer@berkeley.edu>
2017-07-05 09:33:13 -0700

    Added more modelling functionality to APAnalysis
    1. plots negative control profiles
    2. applies shadows modeling
    3. fits langmuir isotherms to 1a3 data

--- APAnalysis.m
+++ APAnalysis.m
@@ -26,7 +26,10 @@ function APAnalysis(dataset, controlset)
 %
 %Author (contact): Armando Reimer (areimer@berkeley.edu)
 %Created: 6/3/2017
-%Last Updated: 6/3/17
+%Last Updated: 7/5/17
+%
+%To do:  1) Allow control set to be optional. 
+%        2) Save graphs somewhere automatically 
  
     warning('off', 'MATLAB:handle_graphics:exceptions:SceneNode')
     
@@ -71,7 +74,7 @@ function APAnalysis(dataset, controlset)
          cumstde(i) = cumstd(i) /  sqrt(sum(cum(:,i) ~= 0));
     end
 
-    figure(1)
+    figure()
     clf('reset')
     for i = 1:nsets
         plot(ap, cum(i, :), 'LineWidth', 1, 'DisplayName', Prefix{i})
@@ -88,8 +91,39 @@ function APAnalysis(dataset, controlset)
     ylabel('Intensity (A.U.)')
     standardizeFigure(gca, lgd1)
     
-
-    figure(2)
+    
+    %Control fraction on
+    figure()
+    clf('reset')
+    g = zeros(len, 1);
+    g2 = zeros(len, 1);
+    n = zeros(len, 1);
+    for i = 1:nsets0
+        f = data0(i).EllipsesOnAP(:,2)./data0(i).TotalEllipsesAP(:,2);
+        nonanf = f;
+        nonanf(isnan(nonanf))=0;
+        g = g + nonanf;
+        g2 = g2 + nonanf.^2; 
+        n = ~isnan(f) + n;
+        plot(ap, f, 'LineWidth', 1, 'DisplayName', Prefix0{i})
+        hold on
+    end
+    n(~n) = 1;
+    fmean = g./n;
+    fstde = sqrt(g2./n - fmean.^2) ./ sqrt(n);
+    errorbar(ap, fmean, fstde, 'LineWidth', 5, 'DisplayName', 'Mean $\pm$ std. error')
+    hold off
+    lgd2 = legend('show');
+    set(lgd2, 'Interpreter', 'Latex')
+    xlim([.1, .8])
+    ylim([0, 1.3])
+    title('Fraction On Control')
+    xlabel('Fraction EL')
+    ylabel('Fraction on')
+    standardizeFigure(gca, lgd2)
+    
+    %Experiment fraction on
+    figure()
     clf('reset')
     g = zeros(len, 1);
     g2 = zeros(len, 1);
@@ -118,8 +152,9 @@ function APAnalysis(dataset, controlset)
     ylabel('Fraction on')
     standardizeFigure(gca, lgd2)
     
-    %This figure will correct figure (2) using the negative control's  fraction on. 
-    figure(3)
+    
+    %This figure will correct fraction on from the experiment using the negative control's  fraction on. 
+    figure()
     clf('reset')
     g = zeros(len, 1);
     g2 = zeros(len, 1);
@@ -142,6 +177,27 @@ function APAnalysis(dataset, controlset)
     fstde0 = sqrt(g2./n - fmean0.^2) ./ sqrt(n);
     
     fcorr = fmean - fmean0; %first order approximation in the limit of no enhancer coupling
+%%    
+    %Here's the full correction 
+    
+    % KD(f - fo) + f*alpha - r*alpha = 0
+    
+    correctionEqn = @(params) params(1)*(fmean - fmean0) + params(2)*fmean -...
+        params(2)*params(3); %params(1) = KD. params(2) = alpha. params(3) = r
+    initParams = [1, 1, 1]; %will figure out units later   
+    lsqOptions=optimset('Display','none','maxfunevals',10000,'maxiter',10000);
+    [fit, res1, residual, exitflag, output, ~, jacobian] = lsqnonlin(correctionEqn, ...
+        initParams,zeros(1,3),inf(1,3), lsqOptions);
+    KD = fit(1);
+    r = fit(3);
+    confidence_intervals = nlparci(fit,residual,'jacobian',jacobian);
+    errors = zeros(1, length(fit));
+    for i = 1:length(confidence_intervals)
+        errors(i) = abs((abs(confidence_intervals(i, 1)) - abs(confidence_intervals(i, 2)))/2);
+    end
+    relative_errors = abs(errors./fit);  
+    %%
+    
     totalerr = sqrt(fstde0.^2 + fstde.^2);
     
     errorbar(ap, fcorr, totalerr, 'LineWidth', 5, 'DisplayName', 'Mean $\pm$ std. error')
@@ -155,14 +211,78 @@ function APAnalysis(dataset, controlset)
     ylabel('Fraction on corrected')
     standardizeFigure(gca, lgd3)
     
+    %%
+    %Enhancer-specific modelling
+    BcdMax = 120; %nM
+    lambda = .2;
+    Bcd = BcdMax*exp(-ap/lambda); 
+    f1A3 = @(constants) constants(1)*Bcd./(Bcd+constants(2)); %Unitless fraction active nuclei
+    rateCoeff = 1; %mRNA / unit time
+    rate1A3 =  @(constants) rateCoeff*constants(1)*Bcd./(Bcd+constants(2)); %dmRNA/dt as a function of activator concentration
+    
+    if contains(dataset, 'v7')
+        %linear fit to Langmuir's isotherm to get thermodynamic parameters
+        %directly. 1/f = 1/r + (KD/r)Bcd 
+        f1 = (1./fmean(8:end))'; %8th bin is roughly 20% AP where terminal system drops off
+        k = ~isinf(f1);
+        Bcd1 = 1./Bcd(8:end);
+        [p, S] = polyfit(Bcd1(k),f1(k),1);        
+        r = 1/p(1);
+        KD = p(2)*r;
+        fFitLinear = r*(Bcd./(Bcd+KD));
+        figure()
+        plot(ap,fFitLinear)
+        title('v7 Fit')
+        xlabel('Fraction EL')    
+        hold on
+        plot(ap, fmean)
+        legend('fit', 'data')
+
+        %not sure why the linear fit results were bad. gonna try nonlinear
+        fv7 = @(constants) constants(1)*Bcd(8:end)./(Bcd(8:end)+constants(2)) - fmean(8:end); %Unitless fraction active nuclei
+        initParams = [1, .3];  
+        lsqOptions=optimset('Display','none','maxfunevals',10000,'maxiter',10000);
+        [fit, res1, residual, exitflag, output, ~, jacobian] = lsqnonlin(fv7, ...
+            initParams,[1,.9],[Inf,120]);
+        r_nonlin = fit(1)
+        KD_nonlin = fit(2)
+        fFitNonLinearBicoid = r_nonlin*(Bcd./(Bcd+KD_nonlin));    
+        fFitNonLinearAP = r_nonlin*(BcdMax*exp(-ap/lambda)./(BcdMax*exp(-ap/lambda)+KD_nonlin));     
+        figure()
+        plot(Bcd,fFitNonLinearBicoid)
+        title('v7 nonlinear fit')
+        xlabel('Bcd (nM)')  
+        ylabel('Fraction Active')
+        figure()
+        plot(ap,fFitNonLinearAP)
+        title('v7 nonlinear fit')
+        xlabel('Fraction EL')  
+        ylabel('Fraction Active')
+        hold on
+        plot(ap, fmean)
+        legend('fit', 'data')
+
+    else
+        figure()
+        plot(ap,f1A3([KD, r]))
+        title('Fitted 1A3 Fraction On')
+        xlabel('Fraction EL')
+        figure()
+        plot(ap, rate1A3([KD, r]))
+        title('Fitted 1A3 dmRNA/dt')
+        xlabel('Fraction EL')
+    end
+    %%
    %Plotting the time projection(s) 
-   for i = 1:nsets
-       figure()
-       stringTemp = Prefix{i};
-       currentPrefix = stringTemp(11:length(stringTemp)-1);
-       [maxTProj, medianTProj] = timeProjection(currentPrefix,'Justnc13');
-       imshow(maxTProj,[0 80],'Border','Tight')
-       title(Prefix{i})
-   end
+%    for i = 1:nsets
+%        figure()
+%        stringTemp = Prefix{i};
+%        currentPrefix = stringTemp(11:length(stringTemp)-1);
+%        [maxTProj, medianTProj] = timeProjection(currentPrefix,'Justnc13');
+%        imshow(maxTProj,[0 80],'Border','Tight')
+%        title(Prefix{i})
+%    end
+   
+   
    
 end
