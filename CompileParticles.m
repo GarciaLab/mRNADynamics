d8fddcbefdec94167761ccae7635ef8ba60fe5ac
yangjun9095 <yjkim90@berkeley.edu>
2017-10-23 14:43:37 -0700

    Support for eGFP-Bcd without His-RFP dataset

--- CompileParticles.m
+++ CompileParticles.m
@@ -260,7 +260,7 @@ Dashes=findstr(Prefix,'-');
 
 %Find the corresponding entry in the XLS file
 if (~isempty(findstr(Prefix,'Bcd')))&(isempty(findstr(Prefix,'BcdE1')))&...
-        (isempty(findstr(Prefix,'NoBcd')))&(isempty(findstr(Prefix,'Bcd1x')))
+        (isempty(findstr(Prefix,'NoBcd')))&(isempty(findstr(Prefix,'Bcd1x')))&(~isempty(findstr(Prefix,'HisRFP')))
     XLSEntry=find(strcmp(Txt(:,DataFolderColumn),...
         [Date,'\BcdGFP-HisRFP']));
 else
