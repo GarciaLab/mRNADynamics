558857842960d1bcfec0f250e6417d73dde0bbba
ArmandoReimer <areimer@berkeley.edu>
2017-11-06 18:32:22 -0800

    more updates on image processing

--- gauss3D.m
+++ gauss3D.m
@@ -1,11 +1,14 @@
-function G = gauss3D(sigma)
+function G = gauss3D(sigma, customSize)
 
-    C = 3;
-    M = C*sigma + 1;
+    if isempty(customSize)
+        C = 3;
+        M = C*sigma + 1;
+    else
+        M = customSize{1};
+    end
     x = -M:1:M;
     y = x;
     z = x;
-    sigma = 3;
     [x_, y_,z_] = meshgrid(x,y,z);
     A = (1/(sigma*sqrt(2*pi)))^3;
     G = A*exp((1/2)*(-(x_.^2)/(2*sigma^2) - (y_.^2)/(2*sigma^2) - (z_.^2)/(2*sigma^2)));
