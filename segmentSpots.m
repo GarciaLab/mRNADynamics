1b162e9a4df21e6e3d05c39174bac9c932c69176
yangjun9095 <yjkim90@berkeley.edu>
2017-11-22 17:11:02 -0800

    segmentSpots support inputoutput mode
    segmentSpots can now support the case where channel 2 is the spot channel. (in case of inputoutput)

--- segmentSpots.m
+++ segmentSpots.m
@@ -196,7 +196,22 @@ if justDoG
     
     for q = 1:nCh
 %         h=waitbar(0,'Generating DoG images');
-        nameSuffix=['_ch',iIndex(q,2)];
+        if strcmp(lower(ExperimentType),'inputoutput')
+            if (~isempty(strfind(lower(Channel2),'mcp')))&...
+                    ~isempty(strfind(lower(Channel2),'pcp'))
+                coatChannel=2;
+            elseif (~isempty(strfind(lower(Channel1),'mcp')))&...
+                    ~isempty(strfind(lower(Channel1),'pcp'))
+                coatChannel=1;
+            else
+                error('No MCP or PCP channel detected. Check MovieDatabase.XLSX')
+            end
+            q=coatChannel;
+            nameSuffix= ['_ch',iIndex(q,2)];
+        else
+            nameSuffix = ['_ch',iIndex(q,2)];
+        end
+        
         for current_frame = 1:numFrames
 %             waitbar(current_frame/numFrames,h);
             if displayFigures
@@ -224,7 +239,22 @@ if justDoG
 else       
     thresh = Threshold; %copy so we can change the value of Threshold for each channel iteration
     for q = 1:nCh
-        nameSuffix=['_ch',iIndex(q,2)];
+        if strcmp(lower(ExperimentType),'inputoutput')
+            if (~isempty(strfind(lower(Channel2),'mcp')))&...
+                    ~isempty(strfind(lower(Channel2),'pcp'))
+                coatChannel=2;
+            elseif (~isempty(strfind(lower(Channel1),'mcp')))&...
+                    ~isempty(strfind(lower(Channel1),'pcp'))
+                coatChannel=1;
+            else
+                error('No MCP or PCP channel detected. Check MovieDatabase.XLSX')
+            end
+            q=coatChannel;
+            nameSuffix= ['_ch',iIndex(q,2)];
+        else
+            nameSuffix = ['_ch',iIndex(q,2)];
+        end
+        
         h=waitbar(0,'Segmenting spots');
         Threshold = thresh(q);
         for current_frame = 1:numFrames
