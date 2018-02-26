730c4459cf039baa4c9794870d1677c712139026
yangjun9095 <yjkim90@berkeley.edu>
2018-02-26 15:54:22 -0800

    Revert "Merge branch 'master' of https://github.com/GarciaLab/mRNADynamics"
    This reverts commit 0f99abb76d7deaeb60cd93d067ceaa8239383dd4, reversing
    changes made to 559f8ce1f6982ed47ec48fae5d53b7ca9b26f007.

--- readMovieDatabase.m
+++ readMovieDatabase.m
@@ -3,40 +3,50 @@ function [SourcePath,FISHPath,DropboxFolder,MS2CodePath, PreProcPath,...
 = readMovieDatabase(PrefixOverrideFlag)
 
 %Figure out the initial folders. We'll update the Drobpox one later on in the code.
-[SourcePath,FISHPath,DropboxFolder,MS2CodePath, PreProcPath, configValues, movieDatabasePath]=...
+[SourcePath,FISHPath,DropboxFolder,MS2CodePath, PreProcPath]=...
     DetermineLocalFolders;
 
 %Get the folder with the data
 if ~PrefixOverrideFlag
-    Folder = uigetdir(SourcePath,'Select folder with data');
-
-    %Get the information from the last two folders in the structure
-    SlashPositions = strfind(Folder,filesep);
-    Prefix = [Folder((SlashPositions(end-1)+1):(SlashPositions(end)-1)),'-',...
-        Folder((SlashPositions(end)+1):(end))];
+    Folder=uigetdir(SourcePath,'Select folder with data');
 else 
     Folder = '';
+end
+
+%Get the information from the last two folders in the structure
+if ~PrefixOverrideFlag
+    SlashPositions=strfind(Folder,filesep);
+    
+    Prefix=[Folder((SlashPositions(end-1)+1):(SlashPositions(end)-1)),'-',...
+        Folder((SlashPositions(end)+1):(end))];
+else 
     Prefix = PrefixOverrideFlag;
 end
 
+%What type of experiment are we dealing with? Get this out of
+%MovieDatabase.xlsx
+[XLSNum,XLSTxt]=xlsread([DropboxFolder,filesep,'MovieDatabase.xlsx']);
+ExperimentTypeColumn=find(strcmp(XLSTxt(1,:),'ExperimentType'));
+DataFolderColumn=find(strcmp(XLSTxt(1,:),'DataFolder'));
+Channel1Column=find(strcmp(XLSTxt(1,:),'Channel1'));
+Channel2Column=find(strcmp(XLSTxt(1,:),'Channel2'));
 
-%What type of experiment are we dealing with? Get this out of MovieDatabase
-movieDatabase = csv2cell(movieDatabasePath, 'fromfile');
-movieDatabaseHeaderRow = movieDatabase(1, :);
-ExperimentTypeColumn = findColumnIndex(movieDatabaseHeaderRow, 'ExperimentType')
-Channel1Column = findColumnIndex(movieDatabaseHeaderRow, 'Channel1')
-Channel2Column = findColumnIndex(movieDatabaseHeaderRow, 'Channel2')
 
-[DropboxFolder, PrefixRow] = getDropboxFolderFromMovieDatabase(movieDatabasePath, Prefix, '[\\\\/-]')
+Dashes=findstr(Prefix,'-');
+PrefixRow=find(strcmp(XLSTxt(:,DataFolderColumn),[Prefix(1:Dashes(3)-1),'\',Prefix(Dashes(3)+1:end)]));
+if isempty(PrefixRow)
+    PrefixRow=find(strcmp(XLSTxt(:,DataFolderColumn),[Prefix(1:Dashes(3)-1),'/',Prefix(Dashes(3)+1:end)]));
+    if isempty(PrefixRow)
+        error('Could not find data set in MovieDatabase.XLSX. Check if it is defined there.')
+    end
+end
 
-ExperimentType = movieDatabase(PrefixRow, ExperimentTypeColumn);
-%ExperimentType = ExperimentType{1}
-Channel1 = movieDatabase(PrefixRow, Channel1Column);
-%Channel1 = Channel1{1}
-Channel2 = movieDatabase(PrefixRow, Channel2Column);
-%Channel2 = Channel2{1}
-
-[~,~,DropboxFolder,~,~] = DetermineLocalFolders(Prefix);
+ExperimentType=XLSTxt(PrefixRow,ExperimentTypeColumn);
+Channel1=XLSTxt(PrefixRow,Channel1Column);
+Channel2=XLSTxt(PrefixRow,Channel2Column);
 
 %Set the destination folders
-OutputFolder = [DropboxFolder, filesep, Prefix]
+OutputFolder=[DropboxFolder,filesep,Prefix];
+
+[~,~,DropboxFolder,~,~]=...
+    DetermineLocalFolders(Prefix);
