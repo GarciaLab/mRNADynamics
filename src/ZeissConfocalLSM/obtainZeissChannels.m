9c8742a5e016b81b2453f2bb1cee6d83c8f4e5be
Matías Potel Feola <harrypotel@gmail.com>
2018-07-27 04:51:26 -0300

    Added support for inputoutput to ZeissConfocalLSM

--- src/ZeissConfocalLSM/obtainZeissChannels.m
+++ src/ZeissConfocalLSM/obtainZeissChannels.m
@@ -1,19 +1,30 @@
 %Figure out the different channels
-function [coatChannel, histoneChannel, fiducialChannel] = obtainZeissChannels(Channel1, Channel2)
-  if ~isempty(strfind(Channel1{1}, 'MCP'))
-    coatChannel = 1;
-  elseif strfind(Channel1{1}, 'His')
-    histoneChannel = 1;
-  else
-    error('LSM Mode error: Channel name not recognized. Check MovieDatabase')
-  end
+function [coatChannel, histoneChannel, fiducialChannel] = obtainZeissChannels(Channel1, Channel2, ExperimentType)
+  
+  if (~strcmpi(ExperimentType, 'inputoutput'))
+    if ~isempty(strfind(Channel1{1}, 'MCP'))
+      coatChannel = 1;
+    elseif strfind(Channel1{1}, 'His')
+      histoneChannel = 1;
+    else
+      error('LSM Mode error: Channel name not recognized. Check MovieDatabase')
+    end
 
-  if ~isempty(strfind(Channel2{1}, 'MCP'))
-    coatChannel = 2;
-  elseif strfind(Channel2{1}, 'His')
-    histoneChannel = 2;
-  else
-    error('LSM Mode error: Channel name not recognized. Check MovieDatabase')
+    if ~isempty(strfind(Channel2{1}, 'MCP'))
+      coatChannel = 2;
+    elseif strfind(Channel2{1}, 'His')
+      histoneChannel = 2;
+    else
+      error('LSM Mode error: Channel name not recognized. Check MovieDatabase')
+    end
+  else 
+    if ~isempty(strfind(Channel1{1}, 'MCP'))
+      coatChannel = 1;
+      histoneChannel = 1;
+    elseif ~isempty(strfind(Channel2{1}, 'MCP'))
+      coatChannel = 2;
+      histoneChannel = 2;
+    end
   end
 
   fiducialChannel = histoneChannel;
