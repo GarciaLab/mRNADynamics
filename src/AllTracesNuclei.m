3c788fd8c8caecd7cef28f830600e957c30ad0d2
Jake Zhao <jiaxi_1@hotmail.com>
2018-09-06 20:05:46 -0700

    Initial Changes for DV pipeline (unchecked)

--- src/AllTracesNuclei.m
+++ src/AllTracesNuclei.m
@@ -1,4 +1,4 @@
-function [AllTracesVector,AllTracesAP]=AllTracesNuclei(FrameInfo,Nuclei)
+function [AllTracesVector,AllTracesAP,AllTracesDV]=AllTracesNuclei(FrameInfo,Nuclei)
 
 %Order all traces in a time array. Also create a vector with the
 %corresponding AP positions
@@ -7,12 +7,14 @@ AllTracesVector=zeros(length(FrameInfo),length(Nuclei));
 AllTracesVector(:)=nan;
 
 AllTracesAP=zeros(length(Nuclei),1);
+AllTracesDV=zeros(length(Nuclei),1);
 
 for i=1:length(Nuclei)
     for j=1:length(Nuclei(i).Frames)
         AllTracesVector(Nuclei(i).Frames(j),i)=...
      Nuclei(i).FluoMax(Nuclei(i).Frames==Nuclei(i).Frames(j));
      AllTracesAP(i)= Nuclei(i).MeanAP;
+     AllTracesDV(i)= Nuclei(i).MeanDV;
     end
    
 end
