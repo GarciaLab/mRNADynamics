2412b0adc8162244600a2b6e2ba8212ff15bce96
Armando Reimer <areimer@berkeley.edu>
2018-09-24 10:49:45 -0700

    lsm channel input no longer case sensitive

--- src/ZeissConfocalLSM/obtainZeissChannels.m
+++ src/ZeissConfocalLSM/obtainZeissChannels.m
@@ -2,26 +2,26 @@
 function [coatChannel, histoneChannel, fiducialChannel] = obtainZeissChannels(Channel1, Channel2, ExperimentType)
   histoneChannel = 0;
   if (~strcmpi(ExperimentType, 'inputoutput'))
-    if ~isempty(strfind(Channel1{1}, 'MCP')) || ~isempty(strfind(Channel1{1}, 'PCP')) || ~isempty(strfind(Channel1{1}, 'PP7'))
+    if ~isempty(strfind(Channel1{1}, lower('MCP'))) || ~isempty(strfind(Channel1{1}, lower('PCP'))) || ~isempty(strfind(Channel1{1}, lower('PP7')))
       coatChannel = 1;
-    elseif strfind(Channel1{1}, 'His')
+    elseif strfind(Channel1{1}, lower('His'))
       histoneChannel = 1;
     else
       error('LSM Mode error: Channel name not recognized. Check MovieDatabase')
     end
 
-    if ~isempty(strfind(Channel2{1}, 'MCP')) || ~isempty(strfind(Channel2{1}, 'PCP')) || ~isempty(strfind(Channel2{1}, 'PP7'))
+    if ~isempty(strfind(Channel2{1}, lower('MCP'))) || ~isempty(strfind(Channel2{1}, lower('PCP'))) || ~isempty(strfind(Channel2{1}, lower('PP7')))
       coatChannel = 2;
-    elseif strfind(Channel2{1}, 'His')
+    elseif strfind(Channel2{1}, lower('His'))
       histoneChannel = 2;
     else
       error('LSM Mode error: Channel name not recognized. Check MovieDatabase')
     end
   else 
-    if ~isempty(strfind(Channel1{1}, 'MCP'))
+    if ~isempty(strfind(Channel1{1}, lower('MCP')))
       coatChannel = 1;
       histoneChannel = 1;
-    elseif ~isempty(strfind(Channel2{1}, 'MCP'))
+    elseif ~isempty(strfind(Channel2{1}, lower('MCP')))
       coatChannel = 2;
       histoneChannel = 2;
     end
