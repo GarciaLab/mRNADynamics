99e1dcdf92c845aab844b58af52aa0ab2162d76d
Matías Potel Feola <harrypotel@gmail.com>
2018-11-10 15:23:34 -0300

    Fixed small bugs with paths in generateDogsWeka and minor modifications to mRNADynamicsTestSuite

--- test/mRNADynamicsTestSuite.m
+++ test/mRNADynamicsTestSuite.m
@@ -1,5 +1,3 @@
-addpath(path, genpath('../testClasses'));
-
 try
   tic;
   fprintf('Test suite started running at %s\n', datestr(now,'yyyy-mm-dd HH:MM:SS.FFF'));
@@ -23,13 +21,5 @@ catch ME
   disp(['Exception: ', ME.identifier]);
   disp(['Message: ', ME.message]);
   disp(['Cause: ', ME.cause]);
-  %exit(1);
+  exit(1);
 end
-
-%We can also do this, if we want to pick and choose the tests
-%suite = TestSuite.fromFile('testKnownExperiments/test2015_07_25_P2P_75uW_bi.m');
-%run(suite);
-%suite = TestSuite.fromFile('testKnownExperiments/test2016_11_13_Hb_P2P_MS2V5_NB_MCP_mCherry.m');
-%run(suite);
-%suite = TestSuite.fromFile('testKnownExperiments/test2017_10_13_20170921_4.m');
-%run(suite);
