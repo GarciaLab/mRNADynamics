781c7067cdb0d9bd15e2264c4251f9647447ff68
Matías Potel Feola <harrypotel@gmail.com>
2018-12-20 00:54:25 -0300

    Refactor for trackmRNADynamics. Moved code to a folder, fixed format and extracted code to common functions either internal or to separate files, whenever possible. Also generated new test case to test no histone path. Removal of thresholds is comming on a new pull request.

---
+++ test/testTrackmRNADynamics/testTrackmRNADynamicsBaseTest.m
@@ -0,0 +1,46 @@
+% Runs the whole pipeline or just segment spots before running the test for trackmRNADynamics
+% in order to allow testing for both paths, histone and not histone.
+function testCase = testTrackmRNADynamicsBaseTest(testCase, histone)
+  tic;
+  disp(['Running trackmRNADynamics test with prefix ', testCase.Prefix]);
+  fprintf('Test run started at %s\n', datestr(now,'yyyy-mm-dd HH:MM:SS.FFF'));
+  
+  %Figure out the initial folders. 
+  CONFIG_CSV_PATH = ['ComputerFolders.csv'];
+  
+  configValues = csv2cell(CONFIG_CSV_PATH, 'fromfile');
+  testPath = getConfigValue(configValues, 'TestPath');
+
+  dynamicResultsPath = getConfigValue(configValues, 'DropboxFolder');
+
+  dynamicResultsExperimentPath = [dynamicResultsPath, filesep, testCase.Prefix];
+  
+  % Precondition, copies existing Expected Data to proper folders before running the process
+  % Depending if the code should use histone or not, it runs track nucleir, or just segment spots
+  TestFolder = 'TrackmRNADynamics';
+  if (histone)
+    copyExpectedDataForPrefix(testCase.Prefix, 'TrackNuclei');
+  else 
+    copyExpectedDataForPrefix(testCase.Prefix, 'SegmentSpotsML');
+    TestFolder = 'TrackmRNADynamicsNoHistone';
+  end
+
+  % Executes trackmRNADynamis with known thresholds
+  TrackmRNADynamics(testCase.Prefix, testCase.Threshold1, testCase.Threshold1, 'bypassUserPrompt');
+
+  expectedDynamicsResultsFolder = [testPath, filesep, TestFolder, filesep, 'DynamicsResults', filesep, testCase.Prefix];
+  assertStructEqualToExpected(testCase, dynamicResultsExperimentPath, expectedDynamicsResultsFolder, 'Particles.mat');
+  assertStructEqualToExpected(testCase, dynamicResultsExperimentPath, expectedDynamicsResultsFolder, 'FrameInfo.mat');
+
+  % Executes tracking a second time to test out the case when Particles.mat already exists
+  TrackmRNADynamics(testCase.Prefix, testCase.Threshold1, testCase.Threshold1, 'bypassUserPrompt');
+
+  expectedDynamicsResultsFolder = [testPath, filesep, TestFolder, filesep, 'secondPass', filesep, 'DynamicsResults',...
+    filesep, testCase.Prefix];
+  assertStructEqualToExpected(testCase, dynamicResultsExperimentPath, expectedDynamicsResultsFolder, 'Particles.mat');
+  assertStructEqualToExpected(testCase, dynamicResultsExperimentPath, expectedDynamicsResultsFolder, 'FrameInfo.mat');
+
+  elapsedTime = toc;
+  fprintf('Test run for %s ended successfully at %s\n', testCase.Prefix, datestr(now,'yyyy-mm-dd HH:MM:SS.FFF'));
+  fprintf('Elapsed time for test was %d minutes and %f seconds\n', floor(elapsedTime/60), rem(elapsedTime,60));
+end
