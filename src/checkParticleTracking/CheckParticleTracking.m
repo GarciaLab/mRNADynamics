658f3fdb483ce5622d19d386327a47d650f7be90
Paul Marchando <pmmflip@gmail.com>
2019-01-11 15:01:12 -0800

    Revert "Revert "Revert "uifigure now closes with everything else in CheckParticleTracking"""
    This reverts commit d5a1d781ddf0dad054c525ec6739d5b028d0dccd.

--- src/checkParticleTracking/CheckParticleTracking.m
+++ src/checkParticleTracking/CheckParticleTracking.m
@@ -112,7 +112,7 @@ function [Particles, Spots, SpotFilter, schnitzcells] = CheckParticleTracking(va
 % Created:
 % Last Updated: 1/13/2018
 
-delete(findall(0));
+close all
 
 
 warning('off','MATLAB:nargchk:deprecated')
@@ -1091,14 +1091,13 @@ else
     save([DataFolder,filesep,'Particles.mat'],'Particles','SpotFilter', '-v7.3')
     save([DataFolder,filesep,'Spots.mat'],'Spots','-v7.3')
 end
-delete(findall(0));
+close all;
 disp('Particles saved.')
 disp(['(Left off at Particle #', num2str(CurrentParticle), ')'])
 
 if fit3DGauss
     plot3DGaussiansToAllSpots(Prefix);
 end
-
 %% Extra stuff that is useful in debug mode
 
 %Reset approve status of all approved particles in a certain nc
