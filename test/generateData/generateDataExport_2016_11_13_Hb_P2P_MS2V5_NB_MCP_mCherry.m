c5dfc7c7b91bf2e7446c3b3744180f59136e642e
Matías Potel Feola <harrypotel@gmail.com>
2019-01-15 22:38:47 -0300

    Restructured generate data scripts to run standalone and avoid issues with Matlab locking directories

---
+++ test/generateData/generateDataExport_2016_11_13_Hb_P2P_MS2V5_NB_MCP_mCherry.m
@@ -0,0 +1,2 @@
+exportTest = testExport_2016_11_13_Hb_P2P_MS2V5_NB_MCP_mCherry;
+generateExpectedExportData(exportTest);
---
+++ test/generateData/generateDataExport_2017_10_13_20170921_4.m
@@ -0,0 +1,2 @@
+exportTest = testExport_2017_10_13_20170921_4;
+generateExpectedExportData(exportTest);
