950cedbeacf2eae11b192660ff833ceab5434a0f
jotadepicas <jotadepicas@gmail.com>
2019-03-29 12:16:59 -0300

    two fixes for checkParticleTracking. 1-coatChannels was not being passed to mainLoop function 2-HisOverlayFig and HisOverlayFigAxes were not being defined for movies that do not use HistoneOverlay, since the variables need to be initialized anyways

--- src/checkParticleTracking/CheckParticleTracking.m
+++ src/checkParticleTracking/CheckParticleTracking.m
@@ -434,6 +434,7 @@ function add_spot_pushed(~, ~)
   end
 
   %Figure out channel-specific information
+  coatChannels = []
   if NChannels == 1
 
     if contains(Channel1{1}, 'MCP') || contains(Channel1{1}, 'PCP')
@@ -480,7 +481,7 @@ function add_spot_pushed(~, ~)
   SnippetEdge, traceFigAxes, PreviousChannel, PreviousParticle, lastParticle, HideApprovedFlag, lineFitted,...
   anaphaseInMins, ElapsedTime, plot3DGauss, anaphase, prophase, metaphase, prophaseInMins, metaphaseInMins, DefaultDropboxFolder,...
   correspondingNCInfo, Coefficients, zProfileFigAxes, zTraceAxes, frame_num, z_num, particle_num,...
-  CurrentZ, no_clicking, DataFolder, DropboxFolder,HisOverlayFig,HisOverlayFigAxes);
+  CurrentZ, no_clicking, DataFolder, DropboxFolder,HisOverlayFig,HisOverlayFigAxes, coatChannels);
   end
 
   save([DataFolder, filesep, 'FrameInfo.mat'], 'FrameInfo')
