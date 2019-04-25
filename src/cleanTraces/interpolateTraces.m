65e5c10f6d7d9cb821d802c8dd1ba6ffdd8f4e0c
Sean Medin <seanmedin56@tubby.campus.berkeley.edu>
2019-04-24 17:05:50 -0700

    changed cleanTraces

--- src/cleanTraces/interpolateTraces.m
+++ src/cleanTraces/interpolateTraces.m
@@ -1,25 +1,36 @@
 function [nucleus_struct] = interpolateTraces(nucleus_struct, minDP, ...
-    TresInterp, num_outputs)
+    TresInterp, num_outputs, nan_buffer)
 %CLEANTRACE Summary of this function goes here
 %   Detailed explanation goes here
 
-    
-    if ~exist('TresInterp')
-        TresInterp = round(med_time);
-    end
     interpGrid = 0:TresInterp:50*60;
     %%% Cleaning Parameters
     jump_thresholds = zeros(1, num_outputs);
+    jump_thresholds3 = zeros(1, num_outputs);
+    jump_thresholds5 = zeros(1, num_outputs);
+    jump_thresholds3D = zeros(1, num_outputs);
     for cidx = 1:num_outputs
-        big_jump = prctile([nucleus_struct.(['fluo' num2str(cidx)])],99);
+        fluo_together = [nucleus_struct.fluo];
+        big_jump = prctile(fluo_together(cidx,:),99);
         jump_thresholds(cidx) = big_jump/1.5; % this should be a conservative threshold for single time step increase
+        fluo3_together = [nucleus_struct.fluo3];
+        big_jump3 = prctile(fluo3_together(cidx,:),99);
+        jump_thresholds3(cidx) = big_jump3/1.5;
+        fluo5_together = [nucleus_struct.fluo5];
+        big_jump5 = prctile(fluo5_together(cidx,:),99);
+        jump_thresholds5(cidx) = big_jump5/1.5;
+        fluo3D_together = [nucleus_struct.fluo3D];
+        big_jump3D = prctile(fluo3D_together(cidx,:),99);
+        jump_thresholds3D(cidx) = big_jump3D/1.5;
     end
     for i = 1:length(nucleus_struct) 
         temp = nucleus_struct(i);
         for cidx = 1:num_outputs
-            trace1 = temp.(['fluo' num2str(cidx)]); %Load full trace, including intervening NaN's    
+            trace1 = temp.fluo(cidx,:); %Load full trace, including intervening NaN's   
+            trace3 = temp.fluo3(cidx,:);
+            trace5 = temp.fluo5(cidx,:);
+            trace3D = temp.fluo3D(cidx,:);
             pt_time = temp.time;      
-            quality_flag = 1; % indicates whether trace suitable for inference
 
             if sum(~isnan(trace1)) == 0 
                 t_start = 0;
@@ -28,49 +39,42 @@ function [nucleus_struct] = interpolateTraces(nucleus_struct, minDP, ...
                 t_start = interpGrid(find(interpGrid>=min(pt_time(~isnan(trace1))),1));
                 t_stop = interpGrid(find(interpGrid<=max(pt_time(~isnan(trace1))),1,'last'));
             end
+            
+            
             time_interp = t_start:TresInterp:t_stop;
-
-            if sum(~isnan(trace1)) < minDP
-                quality_flag = 0;
-                trace1_interp = NaN(size(time_interp));
-                time_interp = NaN(size(time_interp));
+            if sum(~isnan(trace1)) > 1
+                [trace1_interp, quality_flag] = processTrace(trace1,nan_buffer,pt_time,...
+                    time_interp,jump_thresholds(cidx),minDP);
             else
-                %Null assumption is that all clusters of 6 or more NaNs are 0s. Smaller
-                %clusters are assumed to have been missed nonzero dps
-                trace1_nans = isnan(trace1);      
-                %Look for clusters of 6 or more NaNs
-                kernel = [1,1,1,1,1];
-                tn_conv = conv(kernel,trace1_nans);
-                tn_conv = tn_conv(3:end-2);
-                z_ids = find(tn_conv==5);
-                z_ids = unique([z_ids-1 z_ids z_ids+1]); % get set of z_ids    
-                trace1(z_ids) = 0; % set clusters to zeros    
-                trace1(trace1<0) = 0; % deal with negative values    
-                % find single dp "blips". These will be replaced via interpolation
-                tr_dd1 = abs([0 diff(diff(trace1)) 0]);
-                trace1(tr_dd1>2*jump_thresholds(cidx)) = NaN;    
-
-                % interpolate remaining NaNs    
-                query_points1 = pt_time(isnan(trace1));
-                interp_t1 = pt_time(~isnan(trace1));
-                interp_f1 = trace1(~isnan(trace1));
-
-                new_f1 = interp1(interp_t1,interp_f1,query_points1);  
-
-                trace1(ismember(pt_time,query_points1)) = new_f1;        
-
-                %%% flag traces with unreasonably large rises or falls    
-                tr_d1 = diff(trace1);    
-                if max(abs(tr_d1)) >= jump_thresholds(cidx)         
-                    quality_flag = 0;        
-                end
-
-                % Interpolate to standardize spacing    
-                trace1_interp = interp1(pt_time,trace1,time_interp);    
+                trace1_interp = nan(1, length(time_interp));
+                quality_flag = 0;
             end
-            nucleus_struct(i).(['fluo_interp' num2str(cidx)]) = trace1_interp;    
-            nucleus_struct(i).(['time_interp' num2str(cidx)]) = time_interp;
-            nucleus_struct(i).(['inference_flag' num2str(cidx)]) = quality_flag;
+            if sum(~isnan(trace3)) > 1
+                [trace3_interp,~] = processTrace(trace3,nan_buffer,pt_time,...
+                    time_interp,jump_thresholds3(cidx),minDP);
+            else
+                trace3_interp = nan(1, length(time_interp));
+            end
+            if sum(~isnan(trace5)) > 1
+                [trace5_interp,~] = processTrace(trace5,nan_buffer,pt_time,...
+                    time_interp,jump_thresholds5(cidx),minDP);
+            else
+                trace5_interp = nan(1, length(time_interp));
+            end
+            if sum(~isnan(trace3D)) > 1
+                [trace3D_interp,~] = processTrace(trace3D,nan_buffer,pt_time,...
+                    time_interp,jump_thresholds3D(cidx),minDP);
+            else
+                trace3D_interp = nan(1, length(time_interp));
+            end
+            
+            nucleus_struct(i).fluo_interp(cidx,:) = trace1_interp; 
+            nucleus_struct(i).fluo3_interp(cidx,:) = trace3_interp;
+            nucleus_struct(i).fluo5_interp(cidx,:) = trace5_interp;
+            nucleus_struct(i).fluo3D_interp(cidx,:) = trace3D_interp;
+            nucleus_struct(i).time_interp(cidx,:) = time_interp;
+            % indicates whether trace suitable for inference
+            nucleus_struct(i).inference_flag(cidx,:) = quality_flag;
         end
         nucleus_struct(i).TresInterp = TresInterp;
         
@@ -82,13 +86,12 @@ function [nucleus_struct] = interpolateTraces(nucleus_struct, minDP, ...
                 init_vec = temp.(field_string);
                 init_time = temp.time;   
                 if numel(init_time) < 2 || numel(time_interp) < 2
-                    nucleus_struct(i).([interp_fields{j} '_interp']) = init_vec;                
+                    nucleus_struct(i).([interp_fields{j} '_interp'])(cidx,:) = init_vec;                
                 else
-                    nucleus_struct(i).([interp_fields{j} '_interp']) = interp1(init_time,init_vec,time_interp);
+                    nucleus_struct(i).([interp_fields{j} '_interp'])(cidx,:) = interp1(init_time,init_vec,time_interp);
                 end
             end
         end     
-        
     end
 end
 
---
+++ src/cleanTraces/processTrace.m
@@ -0,0 +1,39 @@
+function [trace1_interp, approved] = processTrace(trace1,nan_buffer,pt_time,...
+    time_interp,jump_thresh,minDP)
+%PROCESSTRACE Summary of this function goes here
+%   Detailed explanation goes here
+    
+    % edited version of trace1
+    trace1_clean = trace1;
+
+    %Null assumption is that all clusters of 6 or more NaNs are 0s. Smaller
+    %clusters are assumed to have been missed nonzero dps
+    trace1_nans = isnan(trace1);      
+    %Look for clusters of 6 or more NaNs
+    kernel = ones(1, nan_buffer * 2 + 1);
+    tn_conv = conv(kernel,trace1_nans);
+    tn_conv = tn_conv(1 + nan_buffer:end-nan_buffer);
+    % finds nan points surrounded by at least nan_buffer nan points
+    z_ids = find(tn_conv==length(kernel)); 
+    trace1_clean(z_ids) = 0; % set clusters to zeros    
+    trace1_clean(trace1_clean<0) = 0; % deal with negative values    
+    % find single dp "blips". These will be replaced via interpolation
+    tr_dd1 = abs([0 diff(diff(trace1_clean)) 0]);
+    trace1_clean(tr_dd1>2*jump_thresh) = NaN;    
+
+    % interpolate remaining NaNs    
+    query_points1 = pt_time(isnan(trace1_clean));
+    interp_t1 = pt_time(~isnan(trace1_clean));
+    interp_f1 = trace1_clean(~isnan(trace1_clean));
+
+    new_f1 = interp1(interp_t1,interp_f1,query_points1);  
+
+    trace1_clean(ismember(pt_time,query_points1)) = new_f1;        
+
+    % Interpolate to standardize spacing    
+    trace1_interp = interp1(pt_time,trace1_clean,time_interp); 
+    
+    % says if trace is good
+    approved =  standardQualityControl(trace1,trace1_clean,minDP,jump_thresh);
+end
+
