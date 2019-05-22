993e87772faf093b341c3fb836bfd9c3574a5d5e
yangjun9095 <yjkim90@berkeley.edu>
2019-05-21 18:43:00 -0700

    my genius solution to armando's mistakes

--- src/segmentSpots/segmentTranscriptionalLoci.m
+++ src/segmentSpots/segmentTranscriptionalLoci.m
@@ -83,101 +83,126 @@ q = parallel.pool.DataQueue;
 afterEach(q, @nUpdateWaitbar);
 p = 1;
 
+if filterMovieFlag
+    saveType = 'none';
+end
+
+isZPadded = false;
+
+if strcmpi(saveType, '.tif')
+    firstdogpath = [DogOutputFolder, filesep, dogStr, Prefix, '_', iIndex(1, 3), '_z', iIndex(1, 2),...
+        nameSuffix,'.tif'];
+    firstDoG = imread(firstdogpath);
+    
+elseif strcmpi(saveType, '.mat')
+    firstdogpath = [DogOutputFolder, filesep, dogStr, Prefix, '_', iIndex(1, 3), '_z', iIndex(1, 2),...
+        nameSuffix,'.mat'];
+    load(firstdogpath, 'plane');
+    firstDoG = plane;
+elseif strcmpi(saveType, 'none')
+    firstDoG = dogs(:, :, 1, 1);
+end
+
+if sum(firstDoG(:)) == 0
+    isZPadded = true;
+end
+
+
 for current_frame = initialFrame:numFrames
     
-    
     for zIndex = 1:zSize
-        if zIndex ~= 1 & zIndex ~= zSize
-            
-            imFileName = [PreProcPath, filesep, Prefix, filesep, Prefix, '_', iIndex(current_frame, 3), '_z', iIndex(zIndex, 2),...
-                nameSuffix, '.tif'];
-            im = double(imread(imFileName));
-            try
-                imAbove = double(imread([PreProcPath, filesep, Prefix, filesep, Prefix, '_', iIndex(current_frame, 3), '_z', iIndex(zIndex-1, 2),...
-                    nameSuffix, '.tif']));
-                imBelow = double(imread([PreProcPath, filesep, Prefix, filesep, Prefix, '_', iIndex(current_frame, 3), '_z', iIndex(zIndex+1, 2),...
-                    nameSuffix, '.tif']));
-            catch
-                imAbove = nan(size(im,1),size(im,2));
-                imBelow = nan(size(im,1),size(im,2));
-            end
-            
-            if ~filterMovieFlag
-                try
-                    if ~saveAsMat
-                        dogFileName = [DogOutputFolder, filesep, dogStr, Prefix, '_', iIndex(current_frame, 3), '_z', iIndex(zIndex, 2),...
-                            nameSuffix,'.tif'];
-                        dog = double(imread(dogFileName));
-                    else
-                        if zIndex == 1 || zIndex == zSize
-                            dog = zeros(size(dogs,1), size(dogs, 2));
-                        else
-                            dogFileName = [DogOutputFolder, filesep, dogStr, Prefix, '_', iIndex(current_frame, 3), '_z', iIndex(zIndex-1, 2),...
-                                nameSuffix,'.mat'];
-                            load(dogFileName, 'plane');
-                            dog = plane;
-                        end
-                    end
-                catch
-                    error('Please run filterMovie to create DoG files  ')
+        
+        imFileName = [PreProcPath, filesep, Prefix, filesep, Prefix, '_', iIndex(current_frame, 3), '_z', iIndex(zIndex, 2),...
+            nameSuffix, '.tif'];
+        im = double(imread(imFileName));
+        try
+            imAbove = double(imread([PreProcPath, filesep, Prefix, filesep, Prefix, '_', iIndex(current_frame, 3), '_z', iIndex(zIndex-1, 2),...
+                nameSuffix, '.tif']));
+            imBelow = double(imread([PreProcPath, filesep, Prefix, filesep, Prefix, '_', iIndex(current_frame, 3), '_z', iIndex(zIndex+1, 2),...
+                nameSuffix, '.tif']));
+        catch
+            imAbove = nan(size(im,1),size(im,2));
+            imBelow = nan(size(im,1),size(im,2));
+        end
+        
+        
+        if isZPadded
+            dogZ = zIndex;
+        else
+            dogZ = zIndex - 1;
+        end
+                
+        try
+            if isZPadded | ( ~isZPadded & (zIndex~=1 & zIndex~=zSize) )
+                if strcmpi(saveType, '.tif')
+                    dogFileName = [DogOutputFolder, filesep, dogStr, Prefix, '_', iIndex(current_frame, 3), '_z', iIndex(dogZ, 2),...
+                        nameSuffix,'.tif'];
+                    dog = double(imread(dogFileName));
+                elseif strcmpi(saveType, '.mat')
+                    dogFileName = [DogOutputFolder, filesep, dogStr, Prefix, '_', iIndex(current_frame, 3), '_z', iIndex(dogZ, 2),...
+                        nameSuffix,'.mat'];
+                    load(dogFileName, 'plane');
+                    dog = plane;
+                elseif strcmpi(saveType, 'none')
+                    dog = dogs(:,:, dogZ, current_frame);
                 end
             else
-                if zIndex == 1 || zIndex == zSize
-                    dog = zeros(size(dogs,1), size(dogs, 2));
-                else
-                    dog = dogs(:,:, zIndex - 1, current_frame);
+                dog = false(size(im, 1), size(im, 2));
+            end
+        catch
+            error('Please run filterMovie to create DoG files.');
+        end
+        
+        
+        
+        
+        if displayFigures
+            dogO = im(:);
+            lLim = median(dogO);
+            uLim = max(dogO);
+            if lLim ~= uLim
+                imagescUpdate(dogAx, im, [lLim, uLim]);
+            else
+                imagescUpdate(dogAx, im, []);
+            end
+            drawnow;
+        end
+        
+        % Apply flatfield correction
+        if doFF && sum(size(im)==size(ffim))
+            im = im.*ffim;
+        end
+        
+        im_thresh = dog >= Threshold;
+        
+        if Weka
+            se = strel('square', 3);
+            im_thresh = imdilate(im_thresh, se); %thresholding from this classified probability map can produce non-contiguous, spurious Spots{channelIndex}. This fixes that and hopefully does not combine real Spots{channelIndex} from different nuclei
+            im_thresh = im_thresh > 0;
+        end
+        [im_label, n_spots] = bwlabel(im_thresh);
+        centroids = regionprops(im_thresh, 'centroid');
+        
+        temp_frames = {};
+        temp_particles = cell(1, n_spots);
+        
+        if n_spots ~= 0
+            
+            for spotIndex = 1:n_spots
+                centroid = round(centroids(spotIndex).Centroid);
+                tic
+                [temp_particles(spotIndex), Fits] = identifySingleSpot(spotIndex, {im,imAbove,imBelow}, im_label, dog, ...
+                    neighborhood, snippet_size, pixelSize, displayFigures, graphicsHandles, microscope, 0, centroid,MLFlag, intScale, current_frame, spotIndex, zIndex, use_integral_center);
+                Spots(current_frame).Fits = [Spots(current_frame).Fits, Fits];
+            end
+            
+            for spotIndex = 1:n_spots
+                if ~isempty(temp_particles{spotIndex})
+                    temp_frames = [temp_frames, temp_particles(spotIndex)];
                 end
             end
             
-            if displayFigures
-                dogO = im(:);
-                lLim = median(dogO);
-                uLim = max(dogO);
-                if lLim ~= uLim
-                    imagescUpdate(dogAx, im, [lLim, uLim]);
-                else
-                    imagescUpdate(dogAx, im, []);
-                end
-                drawnow;
-            end
-            
-            % Apply flatfield correction
-            if doFF && sum(size(im)==size(ffim))
-                im = im.*ffim;
-            end
-            
-            im_thresh = dog >= Threshold;
-            
-            if Weka
-                se = strel('square', 3);
-                im_thresh = imdilate(im_thresh, se); %thresholding from this classified probability map can produce non-contiguous, spurious Spots{channelIndex}. This fixes that and hopefully does not combine real Spots{channelIndex} from different nuclei
-                im_thresh = im_thresh > 0;
-            end
-            [im_label, n_spots] = bwlabel(im_thresh);
-            centroids = regionprops(im_thresh, 'centroid');
-            
-            temp_frames = {};
-            temp_particles = cell(1, n_spots);
-            
-            if n_spots ~= 0
-                
-                for spotIndex = 1:n_spots
-                    centroid = round(centroids(spotIndex).Centroid);
-                    tic
-                    [temp_particles(spotIndex), Fits] = identifySingleSpot(spotIndex, {im,imAbove,imBelow}, im_label, dog, ...
-                        neighborhood, snippet_size, pixelSize, displayFigures, graphicsHandles, microscope, 0, centroid,MLFlag, intScale, current_frame, spotIndex, zIndex, use_integral_center);
-                    Spots(current_frame).Fits = [Spots(current_frame).Fits, Fits];
-                end
-                
-                for spotIndex = 1:n_spots
-                    if ~isempty(temp_particles{spotIndex})
-                        temp_frames = [temp_frames, temp_particles(spotIndex)];
-                    end
-                end
-                
-                all_frames{current_frame, zIndex} = temp_frames;
-            end
-            
+            all_frames{current_frame, zIndex} = temp_frames;
         end
         
     end
@@ -185,6 +210,7 @@ for current_frame = initialFrame:numFrames
     send(q, current_frame);
     
 end
+
 
 close(waitbarFigure);
 
