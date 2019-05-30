011020c745b751d52268c7c8c08d4136519ffbbe
jotadepicas <jotadepicas@gmail.com>
2019-05-30 03:37:15 -0300

    fix for OME-TIFF export medianprojection

--- src/export/OMETIFF/calculateOMETIFFProjections.m
+++ src/export/OMETIFF/calculateOMETIFFProjections.m
@@ -24,7 +24,7 @@ function Projections = calculateOMETIFFProjections(ProjectionType, NuclearPlanes
     if strcmpi(ProjectionType, 'medianprojection')
       Projection = median(HisSlices, 3);
     elseif strcmpi(ProjectionType, 'middleprojection')
-      Projection = max(HisSlices(:, :, round(NSlices * .50):round(NSlices * .75)), [], 3);
+      Projection = max(HisSlices(:, :, round(zSize * .50):round(zSize * .75)), [], 3);
     elseif strcmpi(ProjectionType, 'maxprojection')
       Projection = max(HisSlices, [], 3);
     else
