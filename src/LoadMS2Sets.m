df38277d32b11ff1f9a69ba227f57618c498a2f8
Armando Reimer <areimer@berkeley.edu>
2019-07-18 08:54:10 -0700

    added 'justPrefixes' option to loadms2sets

--- src/LoadMS2Sets.m
+++ src/LoadMS2Sets.m
@@ -22,20 +22,23 @@ function [Data, Prefixes, resultsFolder] = LoadMS2Sets(DataType, varargin)
 %Last Updated: 1/13/2018. AR
 
 Prefixes = {};
+Data = [];
+resultsFolder = '';
 
 optionalResults = '';
 compareSettings = true;
 noCompiledNuclei = false;
+justPrefixes = false;
 
 for i= 1:length(varargin)
     if strcmpi(varargin{i},'optionalResults')
         optionalResults = varargin{i+1};
     end
-    if strcmpi(varargin{i},'dontCompare')
-        compareSettings = false;
-    end
     if strcmpi(varargin{i}, 'noCompiledNuclei')
         noCompiledNuclei = true;
+    end
+    if strcmpi(varargin{i}, 'justPrefixes')
+        justPrefixes = true;
     end
 end
 
@@ -114,8 +117,8 @@ for i=1:length(CompiledSets)
     
     [~, ExperimentTypeFromDatabase, ExperimentAxisFromDatabase, ~, ~, APResolutionFromDatabase, ~,...
         ~, ~, ~, ~, ~, ~, ~, ~, ~, ~, ~, ~, ~] = getExperimentDataFromMovieDatabase(Prefix, movieDatabase);
-
-
+    
+    
     %Load and check the experiment details consistency
     if ~isempty(ExperimentType)
         if ~strcmpi(ExperimentType, ExperimentTypeFromDatabase)
@@ -158,212 +161,216 @@ for i=1:length(CompiledSets)
     Prefix=SetName((Quotes(1)+1):(Quotes(end)-1));
     Prefixes = [Prefixes, Prefix];
     
-    %Load CompiledParticles if it exists. This constitutes the main part of
-    %the Data output. However, we will later add more information to this
-    %structure as well as use it to check the consistency of the analysis
-    %performed with the different data sets.
-    
+    if ~justPrefixes
+        
+        %Load CompiledParticles if it exists. This constitutes the main part of
+        %the Data output. However, we will later add more information to this
+        %structure as well as use it to check the consistency of the analysis
+        %performed with the different data sets.
+        
+        if exist([DropboxFolder,filesep,Prefix,filesep,'CompiledParticles.mat'],'file')
+            %Need to try this in case there's some incompatibility in terms of the
+            %structures. This is because we might have xls sets that have been
+            %compiled using different versions of CompileParticles.m
+            try
+                DataTemp=load([DropboxFolder,filesep,Prefix,filesep,'CompiledParticles.mat']);
+                DataTemp=orderfields(DataTemp);
+                Data(i)=DataTemp;
+            catch
+                [Data, DataTemp] = addFields(Data, DataTemp);
+                Data(i) = DataTemp;
+            end
+            
+            
+            %         %Load information about the rotation of the zoomed-in image.
+            %         if exist([DropboxFolder,filesep,Prefix,filesep,'APDetection.mat'])
+            %             APDetection=load([DropboxFolder,filesep,Prefix,filesep,'APDetection.mat']);
+            %             try
+            %                 ImageRotation(i)=APDetection.ImageRotation;
+            %             catch
+            %                 error('Image rotation information not found in APDetection.mat. Rerun AddParticlePosition.m')
+            %             end
+            %         elseif strcmpi(ExperimentAxis,'ap')|strcmpi(ExperimentAxis,'dv')
+            %             error(['APDetection.mat not found despite this experiment being on the ',ExperimentAxis,' axis'])
+            %         end
+            
+            
+            
+            if exist([DropboxFolder,filesep,Prefix,filesep,'APDivision.mat'],'file')
+                APDivisions(i)=load([DropboxFolder,filesep,Prefix,filesep,'APDivision.mat']);
+            else
+                warning('APDivisions.mat not found.')
+            end
+            
+            
+            %Fit results assuming the same slopes
+            if exist([DropboxFolder,filesep,Prefix,filesep,'MeanFits.mat'],'file')
+                MeanFits(i)=load([DropboxFolder,filesep,Prefix,filesep,'MeanFits.mat']);
+            else
+                warning('MeanFits.mat not found');
+            end
+            
+            %Linear slope fit results
+            if exist([DropboxFolder,filesep,Prefix,filesep,'MeanLinearFits.mat'],'file')
+                MeanLinearFits(i)=load([DropboxFolder,filesep,Prefix,filesep,'MeanLinearFits.mat']);
+            end
+            
+            % Fit results from the MeanFitAPAsymmetric.m
+            if exist([DropboxFolder,filesep,Prefix,filesep,'MeanFitsV2.mat'],'file')
+                MeanFitsV2(i)=load([DropboxFolder,filesep,Prefix,filesep,'MeanFitsV2.mat']);
+            else
+                warning('MeanFitsV2.mat not found');
+            end
+            
+            % Fit results from the FitTiltedTrapezoids_4Clicks.m
+            if exist([DropboxFolder,filesep,Prefix,filesep,'MeanFitsV3.mat'],'file')
+                MeanFitsV3(i)=load([DropboxFolder,filesep,Prefix,filesep,'MeanFitsV3.mat']);
+            else
+                warning('MeanFitsV3.mat not found');
+            end
+            
+            %Fit results assuming the same slopes
+            if exist([DropboxFolder,filesep,Prefix,filesep,'MeanFitsMCMC.mat'],'file')
+                MeanFitsMCMC(i)=load([DropboxFolder,filesep,Prefix,filesep,'MeanFitsMCMC.mat']);
+            else
+                warning('MeanFitsMCMC.mat not found');
+            end
+            
+            try
+                Schnitzcells(i)=load([DropboxFolder,filesep,Prefix(1:end),filesep,Prefix(1:end),'_lin.mat'], 'schnitzcells');
+            catch
+                warning('_lin.mat not found.');
+            end
+            
+            
+            %Fit to the integrals
+            if exist([DropboxFolder,filesep,Prefix,filesep,'FitIntegralResults.mat'],'file')
+                IntegralFits(i)=load([DropboxFolder,filesep,Prefix,filesep,'FitIntegralResults.mat']);
+            end
+            
+            
+            %Fits to individual traces
+            if exist([DropboxFolder,filesep,Prefix,filesep,'IndividualFits.mat'],'file')
+                IndividualFits(i)=load([DropboxFolder,filesep,Prefix,filesep,'IndividualFits.mat']);
+            end
+            
+            %Integrated amount accounting from degradation. This is generated using
+            %Jacques' code
+            %     if exist([DropboxFolder,filesep,Prefix,filesep,'AccumulationData.mat'])
+            %         AccumulationData(i)=load([DropboxFolder,filesep,Prefix,filesep,'AccumulationData.mat']);
+            %     end
+            
+            if exist([DropboxFolder,filesep,Prefix,filesep,'MeanFitsUp.mat'],'file')
+                MeanFitsUp(i)=load([DropboxFolder,filesep,Prefix,filesep,'MeanFitsUp.mat']);
+            end
+            
+            if exist([DropboxFolder,filesep,Prefix,filesep,'MeanLinearFitsUp.mat'],'file')
+                MeanLinearFitsUp(i)=load([DropboxFolder,filesep,Prefix,filesep,'MeanLinearFitsUp.mat']);
+            end
+            
+            
+            %Load Ellipses
+            try
+                Ellipses(i)=load([DropboxFolder,filesep,Prefix,filesep,'Ellipses.mat'], 'Ellipses');
+            catch
+                warning('Ellipses.mat not found.')
+            end
+            
+            % Load Particles
+            load([DropboxFolder,filesep,Prefix,filesep,'Particles.mat']);
+            ParticleTemp(i).Particles=Particles;
+            %Count ellipses
+            if exist([DropboxFolder,filesep,Prefix,filesep,'CountEllipses.mat'],'file')
+                CountEllipses(i)=load([DropboxFolder,filesep,Prefix,filesep,'CountEllipses.mat']);
+            else
+                %This is not the smarter way to do this. It relies on having at the
+                %end a set that has been analyzed
+                %CountEllipses(i)=[];
+            end
+        end
+        
+        
+        %Load CompiledNuclei if it exists
+        if exist([DropboxFolder,filesep,Prefix,filesep,'CompiledNuclei.mat'],'file') & ~noCompiledNuclei
+            DataNuclei(i)=load([DropboxFolder,filesep,Prefix,filesep,'CompiledNuclei.mat']);
+        end
+        
+    end
+end
+
+if ~justPrefixes
+    %Now add the SetName and APDivision information
     if exist([DropboxFolder,filesep,Prefix,filesep,'CompiledParticles.mat'],'file')
-        %Need to try this in case there's some incompatibility in terms of the
-        %structures. This is because we might have xls sets that have been
-        %compiled using different versions of CompileParticles.m
-        try
-            DataTemp=load([DropboxFolder,filesep,Prefix,filesep,'CompiledParticles.mat']);
-            DataTemp=orderfields(DataTemp);
-            Data(i)=DataTemp;
-        catch
-            [Data, DataTemp] = addFields(Data, DataTemp);
-            Data(i) = DataTemp;
-        end
-        
-        
-        %         %Load information about the rotation of the zoomed-in image.
-        %         if exist([DropboxFolder,filesep,Prefix,filesep,'APDetection.mat'])
-        %             APDetection=load([DropboxFolder,filesep,Prefix,filesep,'APDetection.mat']);
-        %             try
-        %                 ImageRotation(i)=APDetection.ImageRotation;
-        %             catch
-        %                 error('Image rotation information not found in APDetection.mat. Rerun AddParticlePosition.m')
-        %             end
-        %         elseif strcmpi(ExperimentAxis,'ap')|strcmpi(ExperimentAxis,'dv')
-        %             error(['APDetection.mat not found despite this experiment being on the ',ExperimentAxis,' axis'])
-        %         end
-        
-        
-        
-        if exist([DropboxFolder,filesep,Prefix,filesep,'APDivision.mat'],'file')
-            APDivisions(i)=load([DropboxFolder,filesep,Prefix,filesep,'APDivision.mat']);
-        else
-            warning('APDivisions.mat not found.')
-        end
-        
-        
-        %Fit results assuming the same slopes
-        if exist([DropboxFolder,filesep,Prefix,filesep,'MeanFits.mat'],'file')
-            MeanFits(i)=load([DropboxFolder,filesep,Prefix,filesep,'MeanFits.mat']);
-        else
-            warning('MeanFits.mat not found');
-        end
-        
-        %Linear slope fit results
-        if exist([DropboxFolder,filesep,Prefix,filesep,'MeanLinearFits.mat'],'file')
-            MeanLinearFits(i)=load([DropboxFolder,filesep,Prefix,filesep,'MeanLinearFits.mat']);
-        end
-        
-        % Fit results from the MeanFitAPAsymmetric.m
-        if exist([DropboxFolder,filesep,Prefix,filesep,'MeanFitsV2.mat'],'file')
-            MeanFitsV2(i)=load([DropboxFolder,filesep,Prefix,filesep,'MeanFitsV2.mat']);
-        else
-            warning('MeanFitsV2.mat not found');
-        end
-        
-        % Fit results from the FitTiltedTrapezoids_4Clicks.m
-        if exist([DropboxFolder,filesep,Prefix,filesep,'MeanFitsV3.mat'],'file')
-            MeanFitsV3(i)=load([DropboxFolder,filesep,Prefix,filesep,'MeanFitsV3.mat']);
-        else
-            warning('MeanFitsV3.mat not found');
-        end
-        
-        %Fit results assuming the same slopes
-        if exist([DropboxFolder,filesep,Prefix,filesep,'MeanFitsMCMC.mat'],'file')
-            MeanFitsMCMC(i)=load([DropboxFolder,filesep,Prefix,filesep,'MeanFitsMCMC.mat']);
-        else
-            warning('MeanFitsMCMC.mat not found');
-        end
-        
-        try
-            Schnitzcells(i)=load([DropboxFolder,filesep,Prefix(1:end),filesep,Prefix(1:end),'_lin.mat'], 'schnitzcells');
-        catch
-            warning('_lin.mat not found.');
-        end
-        
-        
-        %Fit to the integrals
-        if exist([DropboxFolder,filesep,Prefix,filesep,'FitIntegralResults.mat'],'file')
-            IntegralFits(i)=load([DropboxFolder,filesep,Prefix,filesep,'FitIntegralResults.mat']);
-        end
-        
-        
-        %Fits to individual traces
-        if exist([DropboxFolder,filesep,Prefix,filesep,'IndividualFits.mat'],'file')
-            IndividualFits(i)=load([DropboxFolder,filesep,Prefix,filesep,'IndividualFits.mat']);
-        end
-        
-        %Integrated amount accounting from degradation. This is generated using
-        %Jacques' code
-        %     if exist([DropboxFolder,filesep,Prefix,filesep,'AccumulationData.mat'])
-        %         AccumulationData(i)=load([DropboxFolder,filesep,Prefix,filesep,'AccumulationData.mat']);
-        %     end
-        
-        if exist([DropboxFolder,filesep,Prefix,filesep,'MeanFitsUp.mat'],'file')
-            MeanFitsUp(i)=load([DropboxFolder,filesep,Prefix,filesep,'MeanFitsUp.mat']);
-        end
-        
-        if exist([DropboxFolder,filesep,Prefix,filesep,'MeanLinearFitsUp.mat'],'file')
-            MeanLinearFitsUp(i)=load([DropboxFolder,filesep,Prefix,filesep,'MeanLinearFitsUp.mat']);
-        end
-        
-        
-        %Load Ellipses
-        try
-            Ellipses(i)=load([DropboxFolder,filesep,Prefix,filesep,'Ellipses.mat'], 'Ellipses');
-        catch
-            warning('Ellipses.mat not found.')
-        end
-        
-        % Load Particles
-        load([DropboxFolder,filesep,Prefix,filesep,'Particles.mat']);
-        ParticleTemp(i).Particles=Particles;
-        %Count ellipses
-        if exist([DropboxFolder,filesep,Prefix,filesep,'CountEllipses.mat'],'file')
-            CountEllipses(i)=load([DropboxFolder,filesep,Prefix,filesep,'CountEllipses.mat']);
-        else
-            %This is not the smarter way to do this. It relies on having at the
-            %end a set that has been analyzed
-            %CountEllipses(i)=[];
+        for i=1:length(Data)
+            Data(i).SetName=SetNames{i};
+            
+            if exist('ImageRotation','var')
+                Data(i).ImageRotation=ImageRotation(i);
+            end
+            
+            try
+                if exist('APDivisions','var')
+                    Data(i).APDivision=APDivisions(i).APDivision;
+                end
+            end
+            
+            if exist('IntegralFits','var')
+                if i<=length(IntegralFits)
+                    Data(i).IntegralFits=IntegralFits(i).FitResults;
+                end
+            end
+            
+            if exist('IndividualFits','var')
+                if i<=length(IndividualFits)
+                    Data(i).IndividualFits=IndividualFits(i).FitResultsIndiv;
+                end
+            end
+            
+            if exist('AccumulationData','var')
+                if i<=length(AccumulationData)
+                    Data(i).AccumulationData=AccumulationData(i).AcumData;
+                end
+            end
+            
+            if exist('MeanFits','var')
+                if i<=length(MeanFits)
+                    Data(i).MeanFits=MeanFits(i).FitResults;
+                end
+            end
+            
+            if exist('MeanFitsV2','var')
+                if i<=length(MeanFitsV2)
+                    Data(i).MeanFitsV2=MeanFitsV2(i).FitResults;
+                end
+            end
+            
+            if exist('MeanLinearFits','var')
+                if i<=length(MeanLinearFits)
+                    Data(i).MeanLinearFits=MeanLinearFits(i).FitResults;
+                end
+            end
+            
+            if exist('MeanLinearFitsUp','var')
+                if i<=length(MeanLinearFitsUp)
+                    Data(i).MeanLinearFitsUp=MeanLinearFitsUp(i).FitResults;
+                end
+            end
+            if exist('MeanFitsMCMC','var')
+                if i<=length(MeanFitsMCMC)
+                    Data(i).MeanFitsMCMC=MeanFitsMCMC(i).MCMCresults;
+                end
+            end
+            try
+                Data(i).schnitzcells=Schnitzcells(i).schnitzcells;
+                Data(i).Ellipses=Ellipses(i).Ellipses;
+            catch
+                warning('No schnitzcells or ellipses');
+            end
+            
+            Data(i).Particles=ParticleTemp(i).Particles;
         end
     end
     
-    
-    %Load CompiledNuclei if it exists
-    if exist([DropboxFolder,filesep,Prefix,filesep,'CompiledNuclei.mat'],'file') & ~noCompiledNuclei
-        DataNuclei(i)=load([DropboxFolder,filesep,Prefix,filesep,'CompiledNuclei.mat']);
-    end
-end
-
-
-%Now add the SetName and APDivision information
-if exist([DropboxFolder,filesep,Prefix,filesep,'CompiledParticles.mat'],'file')
-    for i=1:length(Data)
-        Data(i).SetName=SetNames{i};
-        
-        if exist('ImageRotation','var')
-            Data(i).ImageRotation=ImageRotation(i);
-        end
-        
-        try
-            if exist('APDivisions','var')
-                Data(i).APDivision=APDivisions(i).APDivision;
-            end
-        end
-        
-        if exist('IntegralFits','var')
-            if i<=length(IntegralFits)
-                Data(i).IntegralFits=IntegralFits(i).FitResults;
-            end
-        end
-        
-        if exist('IndividualFits','var')
-            if i<=length(IndividualFits)
-                Data(i).IndividualFits=IndividualFits(i).FitResultsIndiv;
-            end
-        end
-        
-        if exist('AccumulationData','var')
-            if i<=length(AccumulationData)
-                Data(i).AccumulationData=AccumulationData(i).AcumData;
-            end
-        end
-        
-        if exist('MeanFits','var')
-            if i<=length(MeanFits)
-                Data(i).MeanFits=MeanFits(i).FitResults;
-            end
-        end
-        
-        if exist('MeanFitsV2','var')
-            if i<=length(MeanFitsV2)
-                Data(i).MeanFitsV2=MeanFitsV2(i).FitResults;
-            end
-        end
-        
-        if exist('MeanLinearFits','var')
-            if i<=length(MeanLinearFits)
-                Data(i).MeanLinearFits=MeanLinearFits(i).FitResults;
-            end
-        end
-        
-        if exist('MeanLinearFitsUp','var')
-            if i<=length(MeanLinearFitsUp)
-                Data(i).MeanLinearFitsUp=MeanLinearFitsUp(i).FitResults;
-            end
-        end
-        if exist('MeanFitsMCMC','var')
-            if i<=length(MeanFitsMCMC)
-                Data(i).MeanFitsMCMC=MeanFitsMCMC(i).MCMCresults;
-            end
-        end
-        try
-            Data(i).schnitzcells=Schnitzcells(i).schnitzcells;
-            Data(i).Ellipses=Ellipses(i).Ellipses;
-        catch
-            warning('No schnitzcells or ellipses');
-        end
-        
-        Data(i).Particles=ParticleTemp(i).Particles;
-    end
-end
-
 % Add information to DataNuclei if it exists
 if exist('DataNuclei','var')
     for i=1:length(DataNuclei)
@@ -394,10 +401,11 @@ end
 if noCompiledNuclei
     DataTemp = Data;
     clear Data
-      for i=1:length(DataTemp)
-            Data(i).Particles=DataTemp(i);
-      end
+    for i=1:length(DataTemp)
+        Data(i).Particles=DataTemp(i);
+    end
 end
-    
+
+end
 
 end
