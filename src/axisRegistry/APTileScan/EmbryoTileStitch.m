982af6d18b6902fa2c72b558d3e4da7133bf95bf
gdmartini <martini@berkeley.edu>
2019-12-30 23:15:32 -0800

    Create EmbryoTileStitch.m
    Takes a single TileScan data set and generates stitched images of the full embryo using a combination of user input, metadata position information, and automated minimization of the difference between overlapping tiles.

---
+++ src/axisRegistry/APTileScan/EmbryoTileStitch.m
@@ -0,0 +1,110 @@
+function EmbryoTileStitch(Prefix, ID, varargin)
+% author: Gabriella Martini
+% date: 12/30/19
+% last modified: 12/31/19
+% Function takes full embryo images taken as tile scans on the Leica SP8
+% and stitches them into tifs to be used for defining the AP axis
+% and indentifying AP position. 
+%% 
+
+% Parse inputs
+
+if ~exist('ID', 'var')
+    prompt = ['Enter an "ID" for stitching.',...
+    ' The standard inputs "Mid" and "Surf" will stitch the',...
+    ' Midsagittal and Surface Full Embryo images using the',...
+    ' "MidTile.lif" and "SurfTile.lif" files respectively.'];
+    ID = input(prompt,'s');
+
+end
+ID = [upper(ID(1)), ID(2:end)];
+
+
+FullyAutomate = false;
+StitchManually = false;
+
+x = 1;
+while x <= length(varargin)
+    switch varargin{x}
+        case{'NIterations'}
+            NIterations = varargin{x+1};
+            x = x + 1;
+            fprintf('Number of Iterations: %d\n', NIterations);
+        case {'FullyAutomate'}
+            FullyAutomate = true;
+            fprintf('Stitching fully automated.\n')
+        case {'StitchManually'}
+            StitchManually = true;
+            fprintf('Stitching to be performed manually.\n')
+        case {'MaxStep'}
+            MaxStep = varargin{x+1};
+            x = x+1;
+            fprintf('Max Step Size to be used in stitching loop: %d\n', MaxStep)
+        case{'MaxOverlap'}
+            MaxOverlap = varargin{x+1};
+            x = x+1;
+            fprintf('Max overlap between adjacent tiles to be used in stitching loop: %d\n', RowMaxOverlap)
+    end
+    x = x +1;
+end
+
+if StitchManually && FullyAutomate
+    error(['EmbryoTileStitch cannot be run with both "StitchManually"',...
+        'and "FullyAutomate" being true.']);
+end
+if ~exist('ID', 'var')
+    prompt = ['Enter an "ID" for stitching.',...
+        ' The standard inputs "Mid" and "Surf" will stitch the',...
+        ' Midsagittal and Surface Full Embryo images using the',...
+        ' "MidTile.lif" and "SurfTile.lif" files respectively.'];
+    ID = input(prompt,'s');
+    if length(ID) > 1
+        ID = [upper(ID(1)), ID(2:end)];
+    else
+        ID = upper(ID);
+    end
+end
+if ~exist('NIterations', 'var') && ~StitchManually
+    disp('Using default value of NIterations=100.')
+    NIterations = 100;
+end
+if ~exist('MaxStep', 'var') && ~StitchManually
+    disp('Using default value of MaxStep=2.')
+    MaxStep = 2;
+end
+if ~exist('MaxOverlap', 'var') && ~StitchManually
+    disp('Using default value of MaxOverlap=200.')
+    MaxOverlap = 200;
+end
+
+%% Get the relevant folders for this data set
+
+[RawDynamicsPath, ~, DefaultDropboxFolder, DropboxFolder, MS2CodePath,...
+    PreProcPath,configValues,movieDatabasePath, movieDatabase] =...
+    DetermineAllLocalFolders(Prefix);
+mentDataFromMovieDatabase(Prefix, movieDatabase);
+
+% Parse Date and EmbryoName from Prefix
+Dashes=findstr(Prefix,'-');
+Date=Prefix(1:Dashes(3)-1);
+EmbryoName=Prefix(Dashes(3)+1:end);
+
+
+%% Generate Stitching Information
+
+% Generate Initial Tile Array from Metadata files and raw data
+NewTileArrayFromMetadata(Prefix, ID);
+
+% Get user input to improve initial seed for tile stitching
+if ~FullyAutomate
+    ManualStitchingCorrection(Prefix, ID);
+end
+
+% Finalize Tile Array Stitching Positions using FindStitchingPositions
+if ~StitchManually
+    FindStitchingPositions(Prefix, ID, MaxStep, MaxOverlap, NIterations);
+end
+
+
+
+end
