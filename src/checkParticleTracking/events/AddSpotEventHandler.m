c2f6c9e934e35035b482362bad8d78f24a37220a
Armando Reimer <areimer@berkeley.edu>
2020-01-24 13:48:02 -0800

    that didn't do it. this will

--- src/checkParticleTracking/events/AddSpotEventHandler.m
+++ src/checkParticleTracking/events/AddSpotEventHandler.m
@@ -1,17 +1,29 @@
 function [textInputHandler, keyInputHandler] =...
-    AddSpotEventHandler(cptState, smart_add_spot, PreProcPath, ProcPath, Prefix, robot, fake_event)
- %Add particle and all of its shadows to cptState.Spots.
- 
+    AddSpotEventHandler(cptState, PreProcPath, ProcPath, Prefix, robot, fake_event)
+%Add particle and all of its shadows to cptState.Spots.
+
     function doAddSpot(PreProcPath, ProcPath,Prefix, cc)
         PathPart1 = [PreProcPath, filesep, Prefix, filesep, Prefix, '_'];
         PathPart2 = [cptState.nameSuffix, '.tif'];
         Path3 = [PreProcPath, filesep, Prefix, filesep, Prefix];
         
         [xSize, ySize, pixelSize, zStep, snippet_size, LinesPerFrame, PixelsPerLine,...
-        numFrames, NDigits] = getFrameInfoParams(cptState.FrameInfo);
-
+            nFrames, NSlices] = getFrameInfoParams(cptState.FrameInfo);
+        
+        %See how  many frames we have and adjust the index size of the files to load accordingly
+        if nFrames < 1E3
+            NDigits = 3;
+        elseif nFrames < 1E4
+            NDigits = 4;
+        else
+            error('No more than 10,000 frames supported.')
+        end
+        
         [cptState.SpotFilter, cptState.Particles, cptState.Spots, cptState.PreviousParticle, cptState.CurrentParticle, cptState.ZoomMode, cptState.GlobalZoomMode] = ...
-            addSpot(cptState.ZoomMode, cptState.GlobalZoomMode, cptState.Particles, cptState.CurrentChannel, ...
+            ...
+            addSpot(...
+            ...
+            cptState.ZoomMode, cptState.GlobalZoomMode, cptState.Particles, cptState.CurrentChannel, ...
             cptState.CurrentParticle, cptState.CurrentFrame, cptState.CurrentZ, snippet_size, PixelsPerLine, ...
             LinesPerFrame, cptState.Spots, cptState.ZSlices, PathPart1, PathPart2, Path3, cptState.FrameInfo, pixelSize, ...
             cptState.SpotFilter, cc, xSize, ySize, NDigits, ...
@@ -21,15 +33,15 @@ function [textInputHandler, keyInputHandler] =...
     function textInput(add_spot, event)
         Overlay = ancestor(add_spot, 'figure');
         figure(Overlay);
-
+        
         cc = '{';
         
         if smart_add_spot.Value
             cc = '[';
         end
-
+        
         cptState.no_clicking = true;
-
+        
         doAddSpot(PreProcPath, ProcPath, Prefix, cc);
         
         robot.keyPress(fake_event);
@@ -43,6 +55,6 @@ function [textInputHandler, keyInputHandler] =...
         end
     end
 
-    textInputHandler = @textInput;
-    keyInputHandler = @keyInput;
+textInputHandler = @textInput;
+keyInputHandler = @keyInput;
 end
