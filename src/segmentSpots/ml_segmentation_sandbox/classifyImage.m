745bce3b14e028ebec2cfc9c4ccea3673fd53e60
ArmandoReimer <armandoreimer@gmail.com>
2020-02-29 08:34:35 -0800

    updated fitDorsalActivity to have mwc

--- src/segmentSpots/ml_segmentation_sandbox/classifyImage.m
+++ src/segmentSpots/ml_segmentation_sandbox/classifyImage.m
@@ -17,7 +17,7 @@ function pMap = classifyImage(im, training, varargin)
 %       load('E:\DorsalSynthetics\Data\PreProcessedData\2020-01-21-1Dg-8D_EfEfEf_9\2020-01-21-1Dg-8D_EfEfEf_9_hisMat.Mat')
 %       im = double(squeeze(hisMat(1, :, :)));
 %       arffFile =  'E:\classifiers backup 9-20-19\classifiers\nuclear\70nmgood.arff';
-%       pMap = classifyImage(im, arfffile)
+%       pMap = classifyImage(im, arffFile)
 %
 %   See also GENERATEDOGSWEKA, GENERATEDOGS
 
@@ -26,9 +26,10 @@ function pMap = classifyImage(im, training, varargin)
 displayFigures = false;
 tempPath = fileparts(training);
 classifierPath = '';
+classifierObj = [];
 reSc = false;
 
-for i = 1:(numel(varargin)-1)
+for i = 1:2:(numel(varargin)-1)
     if i ~= numel(varargin)
         eval([varargin{i} '=varargin{i+1};']);
     end
@@ -37,33 +38,34 @@ end
 xDim=size(im, 2); yDim = size(im, 1);
 ni = xDim*yDim;
 
-%% helper functions
-paren = @(x, varargin) x(varargin{:}); %emulates the syntax array(i)(j)
+warning('off', 'MATLAB:Java:DuplicateClass');
 
 %% initialize weka
 javaaddpath('C:\Program Files\Weka-3-8-4\weka.jar','-end');
 
 %% load up training data
-arffloader = javaObject('weka.core.converters.ArffLoader'); %this constructs an object of the arffloader class
+arffloader = javaObject('weka.core.converters.ArffLoader'); %this constructs an object of  the arffloader class
 arffloader.setFile(javaObject('java.io.File',training)); %construct an arff file object
 trainingData= arffloader.getDataSet;
+trainingData.setClassIndex(trainingData.numAttributes - 1);
+
 
 %normalize data to the max of the training set for better classification
 if reSc
     v = trainingData.attributeToDoubleArray(0);
-    im = (range(v)/median(v))* im./max(max(im));      
+    im = 5*im./max(max(im));     
 end
 
 %% load or build classifier from training data
-if ~isempty(classifierPath)
+if ~isempty(classifierObj)
+    classifier = classifierObj;
+elseif ~isempty(classifierPath) 
     classifier = weka.core.SerializationHelper.read(classifierPath);
 else
     classifier = weka.classifiers.trees.RandomForest;
-    trainingData.setClassIndex(trainingData.numAttributes - 1);
-    options = {'-I' '200' '-K' '2' '-S' '-1650757608'};
+    options = {'-I', '200', '-num-slots', '28', '-K', '2', '-S', '-1650757608'};
     classifier.setOptions(options);
     classifier.buildClassifier(trainingData);
-    
     if displayFigures
         classifier.toString
     end
@@ -72,7 +74,8 @@ end
 
 %% generate test data by filtering the image
 
-waitbarFigure = waitbar(0, 'filtering image');
+% waitbarFigure = waitbar(0, 'filtering image');
+% set(waitbarFigure, 'units', 'normalized', 'position', [0.4, .15, .25,.1]);
 
 testMatrix = zeros(ni, trainingData.numAttributes-1);
 attributes = {};
@@ -91,11 +94,11 @@ for i = 0:nAtt
     
     testMatrix(:,i+1) = fim(:);
     
-     waitbar(i/nAtt, waitbarFigure);
+%      waitbar(i/nAtt, waitbarFigure);
     
 end
 
-close(waitbarFigure);
+% close(waitbarFigure);
 
 %now turn data matrix into weka arff
 tempFile = [tempPath, filesep, 'temp.data'];
@@ -103,11 +106,11 @@ save(tempFile,var2str(testMatrix),'-ascii');
 loader = javaObject("weka.core.converters.MatlabLoader");
 loader.setFile(javaObject('java.io.File',tempFile));
 testData = loader.getDataSet;
-testData.insertAttributeAt(trainingData.classAttribute,trainingData.classIndex)
-testData.setClassIndex(testData.numAttributes-1)
+testData.insertAttributeAt(trainingData.classAttribute,trainingData.classIndex);
+testData.setClassIndex(testData.numAttributes-1);
 
 f = javaObject('weka.filters.unsupervised.attribute.Remove');
-f.setInputFormat(trainingData)
+f.setInputFormat(trainingData);
 testData = javaMethod('useFilter','weka.filters.Filter', testData, f); %match test header to training header
 
 nInstances = testData.numInstances;
@@ -117,24 +120,28 @@ if ~compatible
     error(char(testData.equalHeadersMsg(trainingData)));
 end
 
-%this step is unbearably slow. >1min per frame. needs a massive overhaul
+%this step is unbearably slow. ~1min per frame. needs a massive overhaul
 pLin = zeros(testData.numInstances, 1);
-waitbarFigure = waitbar(0, 'classifying image');
-set(waitbarFigure, 'units', 'normalized', 'position', [0.4, .15, .25,.1]);
+
 for i = 1:nInstances
     
-    pLin(i) = paren(classifier.distributionForInstance(testData.instance(i - 1)), 1);
-    %     pLin(i) = paren(c.classifyInstance(rD.instance(i - 1)), 1);
-    waitbar(i/nInstances, waitbarFigure);
+    pLin(i) = classifyInstance(i, testData, classifier);
     
 end
 
-close(waitbarFigure);
 
 pMap = reshape(pLin, [yDim xDim]);
+
 
 if displayFigures
     figure(1); imshowpair(im, pMap, 'montage');
 end
 
 end
+
+ function p = classifyInstance(ind, testData, classifier)
+         k = ind - 1;
+        inst = testData.instance(k);
+        temp = classifier.distributionForInstance(inst);
+        p = temp(1);
+ end
