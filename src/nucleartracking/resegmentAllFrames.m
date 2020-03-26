598666a508b09013494bd7cf580a82acb005d7c0
Simon Alamos <simon.alamos@berkeley.edu>
2020-03-26 09:36:28 -0700

    added size filter to track nuclei

--- src/nucleartracking/resegmentAllFrames.m
+++ src/nucleartracking/resegmentAllFrames.m
@@ -1,8 +1,23 @@
 function resegmentAllFrames(Prefix, varargin)
 
+hisMat = [];
+min_rad_um = 2; %default. good for fly embryos. 
+max_rad_um = 6;
+
+%options must be specified as name, value pairs. unpredictable errors will
+%occur, otherwise.
+for i = 1:2:(numel(varargin)-1)
+    if i ~= numel(varargin)
+        eval([varargin{i} '=varargin{i+1};']);
+    end
+end
+
+
 thisExperiment = liveExperiment(Prefix);
 
-hisMat = getHisMat(thisExperiment);
+if isempty(hisMat)    
+    hisMat = getHisMat(thisExperiment);
+end
 % if exist([thisExperiment.resultsFolder, filesep, 'Ellipses.mat'], 'file')
 %     Ellipses = getEllipses(thisExperiment);
 % else
@@ -12,7 +27,9 @@ Ellipses = cell(thisExperiment.nFrames, 1);
 parfor CurrentFrame = 1:thisExperiment.nFrames
         
         HisImage = hisMat(:, :, CurrentFrame);
-        [~, circles] = kSnakeCircles(HisImage, thisExperiment.pixelSize_nm/1000);    
+        [~, circles] = kSnakeCircles(HisImage,...
+            thisExperiment.pixelSize_nm/1000, 'min_rad_um', min_rad_um,...
+            'max_rad_um', max_rad_um);    
         circles(:, 4) = circles(:, 3);
         circles(:, 5:9) = zeros(size(circles, 1), 5);
         Ellipses{CurrentFrame} = circles;
