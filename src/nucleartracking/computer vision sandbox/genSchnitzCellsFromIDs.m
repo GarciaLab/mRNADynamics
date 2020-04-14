f8616eccf0627468b539baebd7ccac26e2c4b4a2
ArmandoReimer <armandoreimer@gmail.com>
2020-04-14 09:10:29 -0700

    minor thing

--- src/nucleartracking/computer vision sandbox/genSchnitzCellsFromIDs.m
+++ src/nucleartracking/computer vision sandbox/genSchnitzCellsFromIDs.m
@@ -20,10 +20,10 @@
                 
                schnitzcells(k).frames(end+1) = frameIndex;
                 schnitzcells(k).cenx(end+1) = max(tracks(k).kalmanFilter.State(1),0);
-                schnitzcells(k).ceny(end+1) = max(tracks(k).kalmanFilter.State(4), 0);
-                schnitzcells(k).smaj(end+1) = max(tracks(k).kalmanFilter.State(7), 0); 
-                schnitzcells(k).smin(end+1) = max(tracks(k).kalmanFilter.State(10), 0);
-                schnitzcells(k).orientationAngle(end+1) = max(tracks(k).kalmanFilter.State(13), 0);
+                schnitzcells(k).ceny(end+1) = max(tracks(k).kalmanFilter.State(1+3), 0);
+                schnitzcells(k).smaj(end+1) = max(tracks(k).kalmanFilter.State(1+2*3), 0); 
+                schnitzcells(k).smin(end+1) = max(tracks(k).kalmanFilter.State(1+3*3), 0);
+                schnitzcells(k).orientationAngle(end+1) = max(tracks(k).kalmanFilter.State(1+4*3), 0);
 
                 
             end
