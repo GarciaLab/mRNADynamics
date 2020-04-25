d7798b87b372694be222d2c7dd546700071061ca
ArmandoReimer <armandoreimer@gmail.com>
2020-04-24 17:32:04 -0700

    off by one error fix in imreadstack2

--- src/imreadStack2.m
+++ src/imreadStack2.m
@@ -1,22 +1,22 @@
 function stack = imreadStack2(file, yDim, xDim, nPages)
 
-    if contains(file, '.tif')
+if contains(file, '.tif')
     t = Tiff(file,'r');
-
+    
     stack = zeros(yDim, xDim, nPages);
-
-
-    for z = 1:nPages-1
-
-        stack(:, :, z) = read(t); 
+    
+    
+    for z = 1:nPages
+        
+        stack(:, :, z) = read(t);
         nextDirectory(t);
-
+        
     end
     close(t);
-    else
-        stack = load(file); 
-        varName = fieldnames(stack);
-        stack = stack.(varName{1});
-    end
+else
+    stack = load(file);
+    varName = fieldnames(stack);
+    stack = stack.(varName{1});
+end
 
 end
