2eb2ada643dec0c75b57ee6a330587f8f39bc258
ArmandoReimer <armandoreimer@gmail.com>
2020-04-24 17:39:25 -0700

    woops

--- src/imreadStack2.m
+++ src/imreadStack2.m
@@ -1,22 +1,29 @@
 function stack = imreadStack2(file, yDim, xDim, nPages)
 
-if contains(file, '.tif')
-    t = Tiff(file,'r');
-    
-    stack = zeros(yDim, xDim, nPages);
-    
-    
-    for z = 1:nPages
+    if contains(file, '.tif')
+        if false
+            t = Tiff(file,'r');
+
+            stack = zeros(yDim, xDim, nPages);
+
+
+            for z = 0:nPages
+
+                stack(:, :, z+1) = read(t);
+                nextDirectory(t);
+
+            end
+            close(t);
+        end
         
-        stack(:, :, z) = read(t);
-        nextDirectory(t);
+        stack = imreadStack(file);
         
+        
+    else
+        stack = load(file);
+        varName = fieldnames(stack);
+        stack = stack.(varName{1});
     end
-    close(t);
-else
-    stack = load(file);
-    varName = fieldnames(stack);
-    stack = stack.(varName{1});
-end
+    
 
 end
