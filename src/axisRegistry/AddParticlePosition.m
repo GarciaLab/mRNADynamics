7f52ce89c6b12b7c56777729894e30344048fe3d
sydghor <37385793+sydghor@users.noreply.github.com>
2020-05-08 17:50:38 -0700

    Revert "Merge branch 'master' of https://github.com/GarciaLab/mRNADynamics"
    This reverts commit 8dcc022dabbea73f9d082ca63d1ffd24ea57b339, reversing
    changes made to 900d79941ce9629ab489ff2679caa706c728d3f1.

--- src/axisRegistry/AddParticlePosition.m
+++ src/axisRegistry/AddParticlePosition.m
@@ -390,7 +390,7 @@ if ~NoAP
         try
             %3D stack
             hisMat = imreadStack([PreProcPath,filesep,Prefix,filesep,Prefix,'-His.tif']);
-            ZoomImage = hisMat(:, :, end-1);
+            ZoomImage = hisMat(:, :, end-1);    
         catch
             %single planes
             ZoomImage=imread([PreProcPath,filesep,Prefix,filesep,DHis.name]);
@@ -755,27 +755,29 @@ if correctDV
     %for convenience.
 end
 
-try
-    Ellipses = getEllipses(liveExperiment);
-    schnitzcells = getSchnitzcells(liveExperiment);
-    
-    if shouldConvertToAP
-        [EllipsePos, APAngle, APLength]...
-            = convertToFractionalEmbryoLength(Prefix);
-    end
-    for s = 1:length(schnitzcells)
-        for f = 1:length(schnitzcells(s).frames)
-            ellipseInd = schnitzcells(s).cellno(f);
-            schnitzcells(s).APPos(f) = EllipsePos{f}(ellipseInd);
-        end
-    end
-    
-    save2([liveExperiment.resultsFolder, filesep,'Ellipses.mat'], Ellipses);
-    save2([liveExperiment.resultsFolder, filesep,Prefix,'_lin.mat'], schnitzcells);
-    
-catch
-    warning('failed to add AP positions to nuclear structures.')
+Ellipses = getEllipses(liveExperiment);
+schnitzcells = getSchnitzcells(liveExperiment);
+
+Ellipses = addSchnitzIndexToEllipses(Ellipses, schnitzcells);
+if shouldConvertToAP
+    [EllipsePos, APAngle, APLength]...
+        = convertToFractionalEmbryoLength(Prefix);
 end
+for s = 1:length(schnitzcells)
+    for f = 1:length(schnitzcells(s).frames)
+        ellipseInd = schnitzcells(s).cellno(f);
+        schnitzcells(s).APPos(f) = EllipsePos{f}(ellipseInd);
+    end
+end
+
+save([liveExperiment.resultsFolder, filesep,'Ellipses.mat'],'Ellipses');
+if (whos(var2str(schnitzcells)).bytes < 2E9)
+    save([liveExperiment.resultsFolder, filesep,Prefix,'_lin.mat'],'schnitzcells', '-v6');
+else
+    save([liveExperiment.resultsFolder, filesep,Prefix,'_lin.mat'],'schnitzcells', '-v7.3', '-nocompression');
+end
+
+
 
 end
 
