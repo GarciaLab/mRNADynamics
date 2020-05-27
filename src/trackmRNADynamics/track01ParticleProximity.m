c6660af838db8174a00e4990a7e9b29116d1d0f8
nlammers371 <nlammers@berkeley.edu>
2020-05-27 09:55:37 -0700

    Condensed structure and add code to automatically break tracks at the start of new nuclear cycles

--- src/trackmRNADynamics/track01ParticleProximity.m
+++ src/trackmRNADynamics/track01ParticleProximity.m
@@ -11,86 +11,43 @@ function [Particles] = track01ParticleProximity(...
   
   % Extract Time Vector
   TimeVec = [FrameInfo.Time];
-  
+  % Extract vector indicating nuclear cleavage cycle for each frame
+  ncVec = [FrameInfo.nc];
   % Check to see if we have nucleus tracking info
   UseNuclei = ~isempty(schnitzcells);
   SlidingWindowSize = 2; % size of window used for time averaging
   
-  for Channel = 1:NCh
-    
+  for Channel = 1:NCh    
     for CurrentFrame = 1:length(Spots{Channel})
       
       % Get the positions of ALL spots (approved and disapproved)
-      [NewSpotsX, NewSpotsY, NewSpotsZ] = SpotsXYZ(Spots{Channel}(CurrentFrame));            
+      [NewSpotsX, NewSpotsY, NewSpotsZ] = SpotsXYZ(Spots{Channel}(CurrentFrame));
       
-      if CurrentFrame==1 && ~isempty(NewSpotsX)
-        %Initialize the Particles structure 
-        for j = 1:length(NewSpotsX)
-          Particles{Channel}(j).Frame = CurrentFrame;
-          Particles{Channel}(j).Index = j;
-          Particles{Channel}(j).Approved = 0;
-          Particles{Channel}(j).FirstFrame = CurrentFrame; % not sure I'll use these
-          Particles{Channel}(j).LastFrame = CurrentFrame; % not sure I'll use these
-          Particles{Channel}(j).xPos = NewSpotsX(j);
-          Particles{Channel}(j).yPos = NewSpotsY(j);
-          Particles{Channel}(j).zPos = NewSpotsZ(j);
-        end
-
-      elseif ~isempty(NewSpotsX)
-        %If particle structure is not empty, then we must link current particles to
-        %prev particles wherever possible
-        dT = TimeVec(CurrentFrame)-TimeVec(CurrentFrame-1);
-        SearchRadius = SearchRadiusMicrons * sqrt(dT);
-        
-        % if we have nulceus tracking info, calculate average
-        % frame-over-frame shift
-        if UseNuclei
-          % use sliding window to estimate average nucleus movement
-          NucleiDxVec = [];
-          NucleiDyVec = [];
-          NewNucleiX = [];
-          NewNucleiY = [];
-          for i = 1:length(schnitzcells)
-            StopFrame = min([length(Spots{Channel}),CurrentFrame+SlidingWindowSize]);
-            StartFrame = max([1,CurrentFrame-SlidingWindowSize]);
-            FrameIndices = find(ismember(schnitzcells(i).frames,[StartFrame,StopFrame]));
-            if length(FrameIndices)==2
-              NucleiDxVec = [NucleiDxVec diff(schnitzcells(i).cenx(FrameIndices))];
-              NucleiDyVec = [NucleiDyVec diff(schnitzcells(i).ceny(FrameIndices))];
-              NewNucleiX = [NewNucleiX schnitzcells(i).cenx(FrameIndices(1))];
-              NewNucleiY = [NewNucleiY schnitzcells(i).ceny(FrameIndices(1))];
-            end
-          end
-
-          % calculate distance to each nucleus 
-          NucleusWeightMat = NaN(length(NewSpotsX),length(NewNucleiX));
-          for i = 1:length(NewSpotsX)
-            NucleusWeightMat(i,:) = (rand(numel(NewNucleiX),1)*0.05+vecnorm([NewSpotsX(i) NewSpotsY(i)]  - [NewNucleiX' NewNucleiY'], 2, 2)).^-2;
-          end
-          % assign weighted mean bulk displacement to particles
-          SpotBulkDxVec = (sum(repmat(NucleiDxVec,length(NewSpotsX),1).*NucleusWeightMat,2) ./ sum(NucleusWeightMat,2) / (StopFrame-StartFrame+1))';
-          SpotBulkDyVec = (sum(repmat(NucleiDyVec,length(NewSpotsX),1).*NucleusWeightMat,2) ./ sum(NucleusWeightMat,2) / (StopFrame-StartFrame+1))';        
-        else
-          SpotBulkDxVec = zeros(size(NewSpotsX));
-          SpotBulkDyVec = zeros(size(NewSpotsY));
-        end
-        
+      % Check if we're at the start of a new nuclear cycle
+      NewNCFlag = false;
+      if CurrentFrame > 1
+        NewNCFlag = ncVec(CurrentFrame-1)~=ncVec(CurrentFrame);
+      end
+      
+      if ~isempty(NewSpotsX)
+        %If spots were detected for this frame we need to add them to the
+        %Particles structure. Link them to existing particles whenever
+        %possible                        
+        ptFlag = exist('Particles');
         %Get a list of the particles that were present in
         %the previous frame and of their positions.
         ExtantParticles = [];
         PrevSpotsX = [];
         PrevSpotsY = [];        
-
-        for j = 1:length(Particles{Channel})
-
-          if Particles{Channel}(j).Frame(end) == (CurrentFrame - 1)
-            ExtantParticles = [ExtantParticles, j];            
-            PrevSpotsX = [PrevSpotsX, Particles{Channel}(j).xPos(end)];
-            PrevSpotsY = [PrevSpotsY, Particles{Channel}(j).yPos(end)];
+        if ptFlag
+          for j = 1:length(Particles{Channel})
+            if Particles{Channel}(j).Frame(end) == (CurrentFrame - 1)
+              ExtantParticles = [ExtantParticles, j];            
+              PrevSpotsX = [PrevSpotsX, Particles{Channel}(j).xPos(end)];
+              PrevSpotsY = [PrevSpotsY, Particles{Channel}(j).yPos(end)];
+            end
           end
-
         end
- 
         % If there were particles present in the previous frame,
         % then we find their distances to the spots present in
         % the current frame. Otherwise, we create new particles
@@ -100,7 +57,45 @@ function [Particles] = track01ParticleProximity(...
         % particle using the NewParticle array.
         NewParticleFlag = true(size(NewSpotsX));
 
-        if ~isempty(ExtantParticles)
+        if ~isempty(ExtantParticles) && ~NewNCFlag
+          % Generate maximum allowed jump radius between spots
+          dT = TimeVec(CurrentFrame)-TimeVec(CurrentFrame-1);
+          SearchRadius = SearchRadiusMicrons * sqrt(dT);
+          
+          % if we have nulceus tracking info, calculate average
+          % frame-over-frame shift
+          if UseNuclei
+            % use sliding window to estimate average nucleus movement
+            NucleiDxVec = [];
+            NucleiDyVec = [];
+            NewNucleiX = [];
+            NewNucleiY = [];
+            StopFrame = min([length(Spots{Channel}),CurrentFrame+SlidingWindowSize]);
+            StartFrame = max([1,CurrentFrame-SlidingWindowSize]);
+            for i = 1:length(schnitzcells)             
+              FrameFT = ismember(schnitzcells(i).frames,[StartFrame,StopFrame]);
+              CurrFT = ismember(schnitzcells(i).frames,CurrentFrame);
+              if sum(FrameFT)==2
+                NucleiDxVec = [NucleiDxVec diff(schnitzcells(i).cenx(FrameFT))];
+                NucleiDyVec = [NucleiDyVec diff(schnitzcells(i).ceny(FrameFT))];
+                NewNucleiX = [NewNucleiX schnitzcells(i).cenx(CurrFT)];
+                NewNucleiY = [NewNucleiY schnitzcells(i).ceny(CurrFT)];
+              end
+            end
+
+            % calculate distance to each nucleus 
+            NucleusWeightMat = NaN(length(NewSpotsX),length(NewNucleiX));
+            for i = 1:length(NewSpotsX)
+              NucleusWeightMat(i,:) = (rand(numel(NewNucleiX),1)*0.05+vecnorm([NewSpotsX(i) NewSpotsY(i)]  - [NewNucleiX' NewNucleiY'], 2, 2)).^-2;
+            end
+            % assign weighted mean bulk displacement to particles
+            SpotBulkDxVec = (sum(repmat(NucleiDxVec,length(NewSpotsX),1).*NucleusWeightMat,2) ./ sum(NucleusWeightMat,2) / (StopFrame-StartFrame+1))';
+            SpotBulkDyVec = (sum(repmat(NucleiDyVec,length(NewSpotsX),1).*NucleusWeightMat,2) ./ sum(NucleusWeightMat,2) / (StopFrame-StartFrame+1))';        
+          else
+            SpotBulkDxVec = zeros(size(NewSpotsX));
+            SpotBulkDyVec = zeros(size(NewSpotsY));
+          end
+          
           % Calculate the distances between the spots in this frame
           % and those within the particles in the previous
           % frame          
@@ -131,17 +126,20 @@ function [Particles] = track01ParticleProximity(...
 
         % See which spots weren't assigned and add them to the structure as new particles
         NewParticles = find(NewParticleFlag);
-
-        for j = NewParticles
+        TotalParticles = 0;
+        if ptFlag
           TotalParticles = length(Particles{Channel});
-          Particles{Channel}(TotalParticles + 1).Frame = CurrentFrame;
-          Particles{Channel}(TotalParticles + 1).Index = j;
-          Particles{Channel}(TotalParticles + 1).Approved = 0;
-          Particles{Channel}(TotalParticles + 1).FirstFrame = CurrentFrame; % not sure I'll use these
-          Particles{Channel}(TotalParticles + 1).LastFrame = CurrentFrame; % not sure I'll use these
-          Particles{Channel}(TotalParticles + 1).xPos = NewSpotsX(j);
-          Particles{Channel}(TotalParticles + 1).yPos = NewSpotsY(j);
-          Particles{Channel}(TotalParticles + 1).zPos = NewSpotsZ(j);          
+        end
+        for j = NewParticles    
+          TotalParticles = TotalParticles + 1;
+          Particles{Channel}(TotalParticles).Frame = CurrentFrame;
+          Particles{Channel}(TotalParticles).Index = j;
+          Particles{Channel}(TotalParticles).Approved = 0;
+          Particles{Channel}(TotalParticles).FirstFrame = CurrentFrame; % not sure I'll use these
+          Particles{Channel}(TotalParticles).LastFrame = CurrentFrame; % not sure I'll use these
+          Particles{Channel}(TotalParticles).xPos = NewSpotsX(j);
+          Particles{Channel}(TotalParticles).yPos = NewSpotsY(j);
+          Particles{Channel}(TotalParticles).zPos = NewSpotsZ(j);          
         end
       end
 
