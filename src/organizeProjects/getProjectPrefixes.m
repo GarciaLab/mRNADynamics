c1427d3350d72fa11ab5cf9e3e4f768e143dd89d
meghanturner16 <meghanturner62@gmail.com>
2020-07-15 11:34:27 -0700

    getProjectPrefixes now supports custom approval flags in DataStatus
    Note that the approval flag in the columns MUST be a boolean (1s for approved, 0s for unapproved)

--- src/organizeProjects/getProjectPrefixes.m
+++ src/organizeProjects/getProjectPrefixes.m
@@ -15,7 +15,17 @@ function prefixes = getProjectPrefixes(dataType,varargin)
 % 'onlyApproved': Limits the prefixes returned by this function to only
 %                 those that have been marked 'ready' or 'ApproveAll' in
 %                 the CompileParticles row of the DataStatus tab
-% 'onlyUnapproved': As above but Unapproved
+% 'onlyUnapproved': Limits the prefixes returned by this function to only
+%                   those that have NOT been marked 'ready' or 'ApproveAll'
+%                   in the CompileParticles row of the DataStatus tab
+% 'customApproved': Limits the prefixes returned by this function to only
+%                   those that have been marked with a custom approval
+%                   flag.
+%                   Follow with the name of the row in DataStatus that 
+%                   contains the approval flag (which must be a boolean,
+%                   1 for approved and 0 for unapproved)
+%                   e.g. getProjectPrefixes(dataType, 'customApproved',
+%                        'ReadyForEnrichment')
 %
 % OUTPUT
 % prefixes: n x 1 cell array containing the Prefixes for this project tab
@@ -26,6 +36,8 @@ function prefixes = getProjectPrefixes(dataType,varargin)
 
 onlyApproved = false; 
 onlyUnapproved = false;
+customApproved = false;
+customApprovalFlag = '';
 prefixes = {}; %#ok<NASGU>
 dataStatusFilename = 'DataStatus.*';    %This naming convention is now enforced inside findDataStatus.m
 
@@ -35,6 +47,9 @@ for i= 1:length(varargin)
         onlyApproved = true; 
     elseif strcmpi(varargin{i}, 'onlyUnapproved')
         onlyUnapproved = true;
+    elseif strcmpi(varargin{i}, 'customApproved')
+        customApproved = true;
+        customApprovalFlag = varargin{i+1};
     end
 end
 
@@ -59,9 +74,10 @@ dataTypeTabContents = readcell([dropboxFolder,filesep,dataStatusDir(1).name], ..
 %Get the Prefixes for all datasets
 [allPrefixes,~] = getPrefixesFromDataStatusTab(dataTypeTabContents);
 
-%% 'onlyApproved' or 'onlyUnapproved' Option
+
+%% Handle 'Approved' dataset options
+
 if onlyApproved || onlyUnapproved
-    
     %Find which datasets are "ready" or "approved". All others are "ignored".
     compileRow = find(strcmpi(dataTypeTabContents(:,1),'AnalyzeLiveData Compile Particles') |...
                       strcmpi(dataTypeTabContents(:,1),'CompileParticles') |...
@@ -71,7 +87,6 @@ if onlyApproved || onlyUnapproved
                    strcmpi(dataTypeTabContents(compileRow, 2:end),'ApproveAll');
 
     if onlyApproved
-        
         outSets = find(readyLogicalArray);
 
         if isempty(outSets)
@@ -83,16 +98,18 @@ if onlyApproved || onlyUnapproved
     end
         
     outPrefixes = allPrefixes(outSets);
-    
     prefixes = outPrefixes;
+
+elseif customApproved
+    % Find the row in the DataStatus tab that contains the custom approval flag 
+    approvalFlagRow = find(strcmpi(dataTypeTabContents(:,1),customApprovalFlag));
+    % Contents of the approval flag row much be int booleans (1s or 0s)
+    approvalFlagLogicalArray = cell2mat(dataTypeTabContents(approvalFlagRow,2:end));
     
+    approvedPrefixes = allPrefixes(find(approvalFlagLogicalArray));
+    prefixes = approvedPrefixes;
 else
-    
     prefixes = allPrefixes;
-    
 end
-
-
-
 
 end
