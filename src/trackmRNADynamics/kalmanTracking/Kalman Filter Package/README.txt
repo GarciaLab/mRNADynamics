bb11e0f9fcca6b6f7441361b38ca34b1743fff71
nlammers371 <nlammers@berkeley.edu>
2020-10-21 09:36:34 -0700

    Removed obsolete HMM scripts. Beginning Kalman rewrite

--- src/trackmRNADynamics/kalmanTracking/Kalman Filter Package/README.txt
+++
@@ -1,6 +0,0 @@
-Kalman Filter Package Installation Instructions:
-
-Make sure you choose Kalman Filter Package -> Add to Path -> Selected Folders And Subfolders
-
-(Kalman Filter Package requires access to methods contained in its subfolders)
-
--- src/trackmRNADynamics/kalmanTracking/makeParticleTrackAssignment.m
+++ src/trackmRNADynamics/kalmanTracking/makeParticleTrackAssignment.m
@@ -7,13 +7,15 @@ function [assignments, unassignedTracks, unassignedDetections] = ...
   % Compute the cost of assigning each detection to each track.
 
   cost = zeros(nTracks, nDetections);
+  % NL: This returns the negative of  the log likelihood, with 
+  % trajectories modeled as multidimensional Gaussian process
   for k = 1:nTracks
       cost(k, :) = distance(tracks(k).kalmanFilter, measurements);
   end
 
   %AR- i don't know how to properly adjust
   %this number.
-  costOfNonAssignment = nanmin([prctile(min(cost),95) 20]);
+  costOfNonAssignment = -log(1e-3);%nanmin([prctile(min(cost),95) 20]);
   [assignments, unassignedTracks, unassignedDetections] = ...
       assignDetectionsToTracks(cost, costOfNonAssignment);
 
