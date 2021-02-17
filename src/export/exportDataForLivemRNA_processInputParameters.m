eea706697729cf74548f646c01d00080365a0873
ArmandoReimer <armandoreimer@gmail.com>
2021-02-17 12:52:16 -0800

    fixed getcytomcp

--- src/export/exportDataForLivemRNA_processInputParameters.m
+++ src/export/exportDataForLivemRNA_processInputParameters.m
@@ -21,7 +21,6 @@ skipExtraction = false;
 rootFolder = '';
 zslicesPadding = false;
 dataType = '';
-exportNuclearProjections = true;
 exportMovieFiles = true;
 ignoreCh3 = false;
 skipNuclearProjection = true;
@@ -55,11 +54,8 @@ while k<=length(varargin)
         shouldTrackNuclei = varargin{k+1};
     elseif strcmpi(varargin{k}, 'zslicesPadding')
         zslicesPadding = true;
-        
-    elseif strcmpi(varargin{k}, 'exportNuclearProjections')
-        exportNuclearProjections= varargin{k+1};
     elseif strcmpi(varargin{k}, 'skipNuclearProjection')
-        skipNuclearProjection = varargin{k+1};
+        skipNuclearProjection = true;
     elseif strcmpi(varargin{k}, 'exportMovieFiles')
         exportMovieFiles = varargin{k+1};
     else
